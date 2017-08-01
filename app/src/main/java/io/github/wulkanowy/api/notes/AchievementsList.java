ee52d727ebba31a68e4a2e00bafcfd9671a4eb70
Mikołaj Pich <m.pich@outlook.com>
2017-08-02 01:34:22 +0200

    Remove unused import

--- app/src/main/java/io/github/wulkanowy/api/notes/AchievementsList.java
+++ app/src/main/java/io/github/wulkanowy/api/notes/AchievementsList.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.github.wulkanowy.api.StudentAndParent;
 import io.github.wulkanowy.api.login.LoginErrorException;
 
 public class AchievementsList {
