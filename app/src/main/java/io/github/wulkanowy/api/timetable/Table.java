821ca3c11f3eb76df37c881359e64650f37b5004
Mikołaj Pich <m.pich@outlook.com>
2017-08-04 00:23:27 +0200

    Add timetable api

---
+++ app/src/main/java/io/github/wulkanowy/api/timetable/Table.java
@@ -0,0 +1,131 @@
+package io.github.wulkanowy.api.timetable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.wulkanowy.api.StudentAndParent;
+import io.github.wulkanowy.api.Vulcan;
+import io.github.wulkanowy.api.login.LoginErrorException;
+
+public class Table extends Vulcan {
+
+    private Timetable timetable;
+    private StudentAndParent snp;
+
+    public Table(Timetable timetable, StudentAndParent snp) {
+        this.timetable = timetable;
+        this.snp = snp;
+    }
+
+    public Week getWeekTable() throws IOException, LoginErrorException {
+        return getWeekTable("");
+    }
+
+    public Week getWeekTable(String tick) throws IOException, LoginErrorException {
+        Element table = timetable.getTablePageDocument(tick)
+                .select(".mainContainer .presentData").first();
+
+        Elements tableHeaderCells = table.select("thead th");
+        List<Day> days = new ArrayList<>();
+
+        for (int i = 2; i < 7; i++) {
+            String[] dayHeaderCell = tableHeaderCells.get(i).html().split("<br>");
+            boolean isFreeDay = tableHeaderCells.get(i).hasClass("free-day");
+
+            Day day = new Day();
+            day.setDate(dayHeaderCell[1]);
+
+            if (isFreeDay) {
+                day.setFreeDay(isFreeDay);
+                day.setFreeDayName(dayHeaderCell[2]);
+            }
+
+            days.add(day);
+        }
+
+        Elements hoursInDays = table.select("tbody tr");
+
+        // fill days in week with lessons
+        for (Element row : hoursInDays) {
+            Elements hours = row.select("td");
+
+            // fill hours in day
+            for (int i = 2; i < hours.size(); i++) {
+                Lesson lesson = new Lesson();
+
+                Elements e = hours.get(i).select("div");
+                switch (e.size()) {
+                    case 1:
+                        lesson = getLessonFromElement(e.first());
+                        break;
+                    case 3:
+                        lesson = getLessonFromElement(e.get(1));
+                        break;
+                    default:
+                        lesson.setEmpty(true);
+                        break;
+                }
+
+                String[] startEndEnd = hours.get(1).text().split(" ");
+                lesson.setStartTime(startEndEnd[0]);
+                lesson.setEndTime(startEndEnd[1]);
+
+                days.get(i - 2).setLesson(lesson);
+            }
+        }
+
+        Element startDayCellHeader = tableHeaderCells.get(2);
+        String[] dayDescription = startDayCellHeader.html().split("<br>");
+
+        return new Week()
+                .setStartDayDate(dayDescription[1])
+                .setDays(days);
+    }
+
+    public Lesson getLessonFromElement(Element e) {
+        Lesson lesson = new Lesson();
+        Elements spans = e.select("span");
+
+        lesson.setSubject(spans.get(0).text());
+        lesson.setTeacher(spans.get(1).text());
+        lesson.setRoom(spans.get(2).text());
+
+        if ((4 == spans.size() && (spans.first().attr("class").equals(""))
+                || 5 == spans.size()
+                && spans.first().hasClass(Lesson.CLASS_NEW_MOVED_IN_OR_CHANGED))) {
+            lesson.setDivisionIntoGroups(true);
+            String[] subjectNameArray = lesson.getSubject().split(" ");
+            String groupName = subjectNameArray[subjectNameArray.length - 1];
+            lesson.setSubject(lesson.getSubject().replace(" " + groupName, ""));
+            lesson.setGroupName(StringUtils.substringBetween(groupName, "[", "]"));
+            lesson.setTeacher(spans.get(2).text());
+            lesson.setRoom(spans.get(3).text());
+        }
+
+        if (spans.first().hasClass(Lesson.CLASS_MOVED_OR_CANCELED)) {
+            lesson.setMovedOrCanceled(true);
+        } else if (spans.first().hasClass(Lesson.CLASS_NEW_MOVED_IN_OR_CHANGED)) {
+            lesson.setNewMovedInOrChanged(true);
+        } else if (spans.first().hasClass(Lesson.CLASS_PLANNING)) {
+            lesson.setPlanning(true);
+        }
+
+        if (spans.last().hasClass(Lesson.CLASS_REALIZED)
+                || spans.first().attr("class").equals("")) {
+            lesson.setRealized(true);
+        }
+
+        if ((4 == spans.size() || 5 == spans.size())
+                && (spans.first().hasClass(Lesson.CLASS_MOVED_OR_CANCELED)
+                || spans.first().hasClass(Lesson.CLASS_NEW_MOVED_IN_OR_CHANGED))) {
+            lesson.setDescription(StringUtils.substringBetween(spans.last().text(), "(", ")"));
+        }
+
+        return lesson;
+    }
+}
