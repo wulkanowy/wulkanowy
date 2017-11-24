0882eb08537955e3ce65b7d2acafb1b0f71b17ea
RicomenPL <RafalBO99@outlook.com>
2017-11-24 22:07:48 +0100

    Add timetable UI (W.I.P)

--- app/src/main/java/io/github/wulkanowy/activity/dashboard/timetable/TimetableFragment.java
+++ app/src/main/java/io/github/wulkanowy/activity/dashboard/timetable/TimetableFragment.java
@@ -3,45 +3,43 @@ package io.github.wulkanowy.activity.dashboard.timetable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
+import com.mikepenz.fastadapter.IItem;
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter;
+import com.mikepenz.fastadapter.expandable.ExpandableExtension;
 
 import io.github.wulkanowy.R;
-import io.github.wulkanowy.activity.WulkanowyApp;
-import io.github.wulkanowy.api.Vulcan;
-import io.github.wulkanowy.services.LoginSession;
-import io.github.wulkanowy.services.VulcanSynchronization;
-import io.github.wulkanowy.services.jobs.VulcanJobHelper;
 
 public class TimetableFragment extends Fragment {
+
+    private FastItemAdapter<IItem> fastItemAdapter;
+    private ExpandableExtension<IItem> expandableExtension;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timetable, container, false);
 
-        Button button = view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            VulcanSynchronization vulcanSynchronization = new VulcanSynchronization(new LoginSession());
-                            vulcanSynchronization.loginCurrentUser(getContext(), ((WulkanowyApp) getActivity().getApplication()).getDaoSession(), new Vulcan());
-                            vulcanSynchronization.syncTimetable();
-                        } catch (Exception e) {
-                            Log.e(VulcanJobHelper.DEBUG_TAG, "Error", e);
-                        }
-                    }
-                }).start();
-            }
-        });
+        fastItemAdapter = new FastItemAdapter<>();
+        fastItemAdapter.withSelectable(true);
+        expandableExtension = new ExpandableExtension<>();
+        fastItemAdapter.addExtension(expandableExtension);
+
+        RecyclerView recyclerView = view.findViewById(R.id.timetable_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(fastItemAdapter);
 
         return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState = fastItemAdapter.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 }
