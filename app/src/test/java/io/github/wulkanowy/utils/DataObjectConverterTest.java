28340fdba61317e7b991291f74b9832768519034
Mikołaj Pich <m.pich@outlook.com>
2018-02-26 22:39:04 +0100

    Fix app tests

--- app/src/test/java/io/github/wulkanowy/utils/DataObjectConverterTest.java
+++ app/src/test/java/io/github/wulkanowy/utils/DataObjectConverterTest.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 import io.github.wulkanowy.api.grades.Grade;
 import io.github.wulkanowy.api.grades.Subject;
-import io.github.wulkanowy.db.dao.entities.Day;
-import io.github.wulkanowy.db.dao.entities.Lesson;
+import io.github.wulkanowy.api.generic.Day;
+import io.github.wulkanowy.api.generic.Lesson;
 
 public class DataObjectConverterTest {
 
