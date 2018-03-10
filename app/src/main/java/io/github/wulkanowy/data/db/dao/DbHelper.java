9fbf33d820d90c014f239d438f1b3f5d92320afd
Mikołaj Pich <m.pich@outlook.com>
2018-03-11 00:46:14 +0100

    Add database migrations

--- app/src/main/java/io/github/wulkanowy/data/db/dao/DbHelper.java
+++ app/src/main/java/io/github/wulkanowy/data/db/dao/DbHelper.java
@@ -3,33 +3,48 @@ package io.github.wulkanowy.data.db.dao;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.github.yuweiguocn.library.greendao.MigrationHelper;
+
 import org.greenrobot.greendao.database.Database;
 import org.greenrobot.greendao.database.StandardDatabase;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.github.wulkanowy.data.db.dao.entities.AccountDao;
 import io.github.wulkanowy.data.db.dao.entities.DaoMaster;
+import io.github.wulkanowy.data.db.dao.entities.GradeDao;
+import io.github.wulkanowy.data.db.dao.entities.SubjectDao;
 import io.github.wulkanowy.data.db.shared.SharedPrefContract;
 import io.github.wulkanowy.di.annotations.ApplicationContext;
 import io.github.wulkanowy.di.annotations.DatabaseInfo;
 import io.github.wulkanowy.utils.LogUtils;
 
 @Singleton
-public class DbHelper extends DaoMaster.DevOpenHelper {
+public class DbHelper extends DaoMaster.OpenHelper {
 
     private SharedPrefContract sharedPref;
 
     @Inject
     DbHelper(@ApplicationContext Context context, @DatabaseInfo String dbName,
              SharedPrefContract sharedPref) {
-        super(context, dbName);
+        super(context, dbName, null);
         this.sharedPref = sharedPref;
     }
 
     @Override
     public void onUpgrade(Database db, int oldVersion, int newVersion) {
-        cleanUserData(db, oldVersion, newVersion);
+        MigrationHelper.DEBUG = true;
+        MigrationHelper.migrate(db, new MigrationHelper.ReCreateAllTableListener() {
+            @Override
+            public void onCreateAllTables(Database db, boolean ifNotExists) {
+                DaoMaster.createAllTables(db, ifNotExists);
+            }
+            @Override
+            public void onDropAllTables(Database db, boolean ifExists) {
+                DaoMaster.dropAllTables(db, ifExists);
+            }
+        }, AccountDao.class, SubjectDao.class, GradeDao.class);
     }
 
     @Override
