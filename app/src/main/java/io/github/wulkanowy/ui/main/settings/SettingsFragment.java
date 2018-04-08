078a0ad8fdcea47b01a844b6c1d52f595d0f2af4
RicomenPL <RafalBO99@outlook.com>
2018-04-08 13:33:53 +0200

    Change valueOf to parseInt

--- app/src/main/java/io/github/wulkanowy/ui/main/settings/SettingsFragment.java
+++ app/src/main/java/io/github/wulkanowy/ui/main/settings/SettingsFragment.java
@@ -36,7 +36,7 @@ public class SettingsFragment extends PreferenceFragmentCompat
 
     private void launchServices(boolean start, SharedPreferences sharedPref) {
         if (start) {
-            int newInterval = Integer.valueOf(sharedPref.getString(SHARED_KEY_SERVICES_INTERVAL, "60"));
+            int newInterval = Integer.parseInt(sharedPref.getString(SHARED_KEY_SERVICES_INTERVAL, "60"));
             boolean useOnlyWifi = sharedPref.getBoolean(SHARED_KEY_SERVICES_MOBILE_DISABLED, false);
 
             SyncJob.stop(getContext());
