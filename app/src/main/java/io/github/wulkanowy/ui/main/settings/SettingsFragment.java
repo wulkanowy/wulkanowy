6177c1112e6218243ea3139f4b0f3b485540cf95
Mikołaj Pich <m.pich@outlook.com>
2018-05-06 23:28:50 +0200

    Add about section in settings

--- app/src/main/java/io/github/wulkanowy/ui/main/settings/SettingsFragment.java
+++ app/src/main/java/io/github/wulkanowy/ui/main/settings/SettingsFragment.java
@@ -1,8 +1,13 @@
 package io.github.wulkanowy.ui.main.settings;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 
 import io.github.wulkanowy.R;
 import io.github.wulkanowy.services.jobs.SyncJob;
@@ -24,9 +29,25 @@ public class SettingsFragment extends PreferenceFragmentCompat
 
     public static final String SHARED_KEY_SERVICES_MOBILE_DISABLED = "services_disable_mobile";
 
+    public static final String SHARED_KEY_ABOUT_OSL = "about_osl";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        findPreference(SHARED_KEY_ABOUT_OSL).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getActivity(), OssLicensesMenuActivity.class);
+                String title = getString(R.string.pref_about_osl);
+                intent.putExtra("title", title);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     @Override
