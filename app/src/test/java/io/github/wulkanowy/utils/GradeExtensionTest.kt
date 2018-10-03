1b82fe3752540986ea2c7b0507cf0ff5e7a854bc
Rafał Borcz <RafalBO99@outlook.com>
2018-10-03 21:03:42 +0200

    Fix tests

--- app/src/test/java/io/github/wulkanowy/utils/GradeExtensionTest.kt
+++ app/src/test/java/io/github/wulkanowy/utils/GradeExtensionTest.kt
@@ -4,27 +4,30 @@ import io.github.wulkanowy.data.db.entities.Grade
 import io.github.wulkanowy.data.db.entities.GradeSummary
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.threeten.bp.LocalDate
 
 class GradeExtensionTest {
 
     @Test
     fun calcWeightedAverage() {
+        val localDate = mock(LocalDate::class.java)
         assertEquals(3.47, listOf(
                 Grade("", "", "", "", 5, 0.33
                         , "", "", "", "", "",
-                        6, "", ""),
+                        6, localDate, ""),
                 Grade("", "", "", "", 5, -0.33
                         , "", "", "", "", "",
-                        5, "", ""),
+                        5, localDate, ""),
                 Grade("", "", "", "", 4, 0.0
                         , "", "", "", "", "",
-                        1, "", ""),
+                        1, localDate, ""),
                 Grade("", "", "", "", 1, 0.5
                         , "", "", "", "", "",
-                        9, "", ""),
+                        9, localDate, ""),
                 Grade("", "", "", "", 0, 0.0
                         , "", "", "", "", "",
-                        0, "", "")
+                        0, localDate, "")
         ).calcAverage(), 0.005)
     }
 
