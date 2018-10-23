fc6a6c81a6172fb3e6e1775a95b59262b0bfa0bd
Rafał Borcz <RafalBO99@outlook.com>
2018-10-23 22:15:41 +0200

    Add chevron icon

--- app/src/main/java/io/github/wulkanowy/ui/widgets/timetable/TimetableWidgetProvider.kt
+++ app/src/main/java/io/github/wulkanowy/ui/widgets/timetable/TimetableWidgetProvider.kt
@@ -3,11 +3,9 @@ package io.github.wulkanowy.ui.widgets.timetable
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import dagger.android.AndroidInjection
 
 class TimetableWidgetProvider : AppWidgetProvider() {
 
     override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
-        AndroidInjection.inject(this, context)
     }
 }
