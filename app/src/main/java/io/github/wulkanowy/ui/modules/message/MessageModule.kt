2ed684d50a0857a6ec2f2a9996c1e16a3a67853d
Mikołaj Pich <m.pich@outlook.com>
2018-12-04 15:05:59 +0100

    Update di bind method name

--- app/src/main/java/io/github/wulkanowy/ui/modules/message/MessageModule.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/message/MessageModule.kt
@@ -22,5 +22,5 @@ abstract class MessageModule {
 
     @PerChildFragment
     @ContributesAndroidInjector
-    abstract fun bindMessageInboxFragment(): MessageTabFragment
+    abstract fun bindMessageTabFragment(): MessageTabFragment
 }
