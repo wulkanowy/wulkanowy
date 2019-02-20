c0f5f02b64bed340e669004b431e335b388d3c22
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-02-20 21:40:17 +0100

    Make getting the reporting unit from the actual semester

--- app/src/main/java/io/github/wulkanowy/ui/modules/message/send/SendMessagePresenter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/message/send/SendMessagePresenter.kt
@@ -4,6 +4,7 @@ import io.github.wulkanowy.data.db.entities.ReportingUnit
 import io.github.wulkanowy.data.repositories.message.MessageRepository
 import io.github.wulkanowy.data.repositories.recipient.RecipientRepository
 import io.github.wulkanowy.data.repositories.reportingunit.ReportingUnitRepository
+import io.github.wulkanowy.data.repositories.semester.SemesterRepository
 import io.github.wulkanowy.data.repositories.student.StudentRepository
 import io.github.wulkanowy.ui.base.session.BaseSessionPresenter
 import io.github.wulkanowy.ui.base.session.SessionErrorHandler
@@ -15,41 +16,28 @@ class SendMessagePresenter @Inject constructor(
     private val errorHandler: SessionErrorHandler,
     private val schedulers: SchedulersProvider,
     private val studentRepository: StudentRepository,
+    private val semesterRepository: SemesterRepository,
     private val messageRepository: MessageRepository,
     private val reportingUnitRepository: ReportingUnitRepository,
     private val recipientRepository: RecipientRepository
 ) : BaseSessionPresenter<SendMessageView>(errorHandler) {
 
-    private var reportingUnits: List<ReportingUnit> = emptyList()
+    private lateinit var reportingUnit: ReportingUnit
 
     override fun onAttachView(view: SendMessageView) {
         super.onAttachView(view)
         view.initView()
-        loadReportingUnits()
+        loadRecipients()
     }
 
-    private fun loadReportingUnits() {
+    private fun loadRecipients() {
         disposable.add(studentRepository.getCurrentStudent()
-            .flatMap { reportingUnitRepository.getReportingUnits(it) }
-            .filter { !it.isEmpty() }
-            .subscribeOn(schedulers.backgroundThread)
-            .observeOn(schedulers.mainThread)
-            .subscribe({
-                setReportingUnits(it)
-                view?.setReportingUnit(it.first())
-                loadRecipients(it.first())
-            }, {
-                errorHandler.dispatch(it)
-            }, {
-                view?.showProgress(false)
-                errorHandler.dispatch(Exception("Cannot fetch reporting units"))
-            })
-        )
-    }
-
-    private fun loadRecipients(unit: ReportingUnit) {
-        disposable.add(studentRepository.getCurrentStudent()
-            .flatMap { recipientRepository.getRecipients(it, 2, unit) }
+            .flatMapMaybe { student ->
+                semesterRepository.getCurrentSemester(student)
+                    .flatMapMaybe { reportingUnitRepository.getReportingUnit(student, it.unitId) }
+                    .doOnSuccess { reportingUnit = it }
+                    .flatMap { recipientRepository.getRecipients(student, 2, it).toMaybe() }
+            }
             .subscribeOn(schedulers.backgroundThread)
             .observeOn(schedulers.mainThread)
             .doFinally {
@@ -59,16 +47,15 @@ class SendMessagePresenter @Inject constructor(
             }
             .subscribe({
                 view?.apply {
+                    setReportingUnit(reportingUnit)
                     showContent(true)
                 }
-                Timber.i("Fetched " + it.size.toString() + " recipients")
+                Timber.i("Fetched %s recipients", it.size.toString())
             }, {
                 errorHandler.dispatch(it)
+            }, {
+                Timber.e("Couldn't fetch the reporting unit")
             })
         )
-    }
-
-    private fun setReportingUnits(reportingUnits: List<ReportingUnit>) {
-        this.reportingUnits = reportingUnits
     }
 }
