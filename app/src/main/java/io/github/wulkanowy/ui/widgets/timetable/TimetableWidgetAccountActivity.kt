e945e2165df11e288e09d66f1c7d74660ccd4163
RicomenPL <RafalBO99@outlook.com>
2019-03-30 19:51:26 +0100

    Add account list

--- app/src/main/java/io/github/wulkanowy/ui/widgets/timetable/TimetableWidgetAccountActivity.kt
+++ app/src/main/java/io/github/wulkanowy/ui/widgets/timetable/TimetableWidgetAccountActivity.kt
@@ -3,18 +3,52 @@ package io.github.wulkanowy.ui.widgets.timetable
 import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.common.SmoothScrollLinearLayoutManager
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
 import io.github.wulkanowy.R
+import io.github.wulkanowy.data.repositories.student.StudentRepository
 import io.github.wulkanowy.ui.base.BaseActivity
+import io.github.wulkanowy.utils.SchedulersProvider
+import io.github.wulkanowy.utils.setOnItemClickListener
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_widget_account.*
+import javax.inject.Inject
 
 class TimetableWidgetAccountActivity : BaseActivity() {
+
+    private val disposable: CompositeDisposable = CompositeDisposable()
+
+    @Inject
+    lateinit var studentRepository: StudentRepository
+
+    @Inject
+    lateinit var adapter: FlexibleAdapter<AbstractFlexibleItem<*>>
+
+    @Inject
+    lateinit var schedulers: SchedulersProvider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setResult(RESULT_CANCELED)
-        setContentView(R.layout.dialog_account)
+        setContentView(R.layout.activity_widget_account)
+        timetableWidgetAccountActivityRecycler.adapter = adapter
+        timetableWidgetAccountActivityRecycler.layoutManager = SmoothScrollLinearLayoutManager(this)
 
-        intent.extras?.getInt(EXTRA_APPWIDGET_ID)?.let {
-            setResult(RESULT_OK, Intent().apply { putExtra(EXTRA_APPWIDGET_ID, it) })
+        adapter.setOnItemClickListener {
+            intent.extras?.getInt(EXTRA_APPWIDGET_ID)?.let {
+                setResult(RESULT_OK, Intent().apply { putExtra(EXTRA_APPWIDGET_ID, it) })
+            }
+            finish()
         }
+
+        disposable.add(studentRepository.getSavedStudents(false)
+            .subscribeOn(schedulers.backgroundThread)
+            .observeOn(schedulers.mainThread)
+            .map { it.map { student -> TimetableWidgetAccountItem(student) } }
+            .subscribe({
+                adapter.updateDataSet(it)
+            }, { Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show() }))
     }
 }
---
+++ app/src/main/java/io/github/wulkanowy/ui/widgets/timetable/TimetableWidgetAccountItem.kt
@@ -0,0 +1,50 @@
+package io.github.wulkanowy.ui.widgets.timetable
+
+import android.view.View
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import eu.davidea.flexibleadapter.items.IFlexible
+import eu.davidea.viewholders.FlexibleViewHolder
+import io.github.wulkanowy.R
+import io.github.wulkanowy.data.db.entities.Student
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_account.*
+
+class TimetableWidgetAccountItem(val student: Student) : AbstractFlexibleItem<TimetableWidgetAccountItem.ViewHolder>() {
+
+    override fun getLayoutRes() = R.layout.item_account
+
+    override fun createViewHolder(view: View, adapter: FlexibleAdapter<IFlexible<*>>): ViewHolder {
+        return ViewHolder(view, adapter)
+    }
+
+    override fun bindViewHolder(adapter: FlexibleAdapter<IFlexible<*>>, holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        holder.apply {
+            accountItemName.text = student.studentName
+            accountItemSchool.text = student.schoolName
+            accountItemImage.setBackgroundResource(0)
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TimetableWidgetAccountItem
+
+        if (student != other.student) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = student.hashCode()
+        result = 31 * result + student.id.toInt()
+        return result
+    }
+
+    class ViewHolder(view: View, adapter: FlexibleAdapter<*>) : FlexibleViewHolder(view, adapter), LayoutContainer {
+        override val containerView: View
+            get() = contentView
+    }
+}
