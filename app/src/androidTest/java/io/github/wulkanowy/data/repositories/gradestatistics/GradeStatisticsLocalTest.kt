33d7d23c820989274023b24754b640ccd1648796
Mikołaj Pich <m.pich@outlook.com>
2019-04-03 18:50:49 +0200

    Fix tests

--- app/src/androidTest/java/io/github/wulkanowy/data/repositories/gradestatistics/GradeStatisticsLocalTest.kt
+++ app/src/androidTest/java/io/github/wulkanowy/data/repositories/gradestatistics/GradeStatisticsLocalTest.kt
@@ -40,7 +40,7 @@ class GradeStatisticsLocalTest {
         ))
 
         val stats = gradeStatisticsLocal.getGradesStatistics(
-            Semester(2, 2, "", 1, 2, 2019, true, LocalDate.now(), LocalDate.now(), 1, 1), false,
+            Semester(2, 2, "", 2019, 1, 2, true, LocalDate.now(), LocalDate.now(), 1, 1), false,
             "Matematyka"
         ).blockingGet()
         assertEquals(1, stats.size)
@@ -56,7 +56,7 @@ class GradeStatisticsLocalTest {
         ))
 
         val stats = gradeStatisticsLocal.getGradesStatistics(
-            Semester(2, 2, "", 1, 2, 2019, true, LocalDate.now(), LocalDate.now(), 1, 1), false,
+            Semester(2, 2, "", 2019, 1, 2, true, LocalDate.now(), LocalDate.now(), 1, 1), false,
             "Wszystkie"
         ).blockingGet()
         assertEquals(1, stats.size)
