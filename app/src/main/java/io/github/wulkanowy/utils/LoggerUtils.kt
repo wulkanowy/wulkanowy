655f7480816c81952e90ea8f4ed564e735f03abe
Rafał Borcz <RafalBO99@outlook.com>
2019-04-25 13:43:23 +0200

    Add lifecycle logging

--- app/src/main/java/io/github/wulkanowy/utils/LoggerUtils.kt
+++ app/src/main/java/io/github/wulkanowy/utils/LoggerUtils.kt
@@ -1,5 +1,8 @@
 package io.github.wulkanowy.utils
 
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
 import com.crashlytics.android.Crashlytics
 import timber.log.Timber
 
@@ -18,5 +21,36 @@ class CrashlyticsTree : Timber.Tree() {
 
         if (t == null) Crashlytics.log(message)
         else Crashlytics.logException(t)
+    }
+}
+
+class ActivityLifecycleLogger : Application.ActivityLifecycleCallbacks {
+
+    override fun onActivityPaused(activity: Activity?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} PAUSED") }
+    }
+
+    override fun onActivityResumed(activity: Activity?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} RESUMED") }
+    }
+
+    override fun onActivityStarted(activity: Activity?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} STARTED") }
+    }
+
+    override fun onActivityDestroyed(activity: Activity?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} DESTROYED") }
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} SAVED INSTANCE STATE") }
+    }
+
+    override fun onActivityStopped(activity: Activity?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} STOPPED") }
+    }
+
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+        activity?.let { Timber.i("${it::class.java.simpleName} CREATED") }
     }
 }
