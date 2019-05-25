958512996e54da8be369d261cd1c6c3fa516ca00
Mikołaj Pich <m.pich@outlook.com>
2019-05-26 01:32:43 +0200

    Suppress unused parameters

--- app/src/fdroid/java/io/github/wulkanowy/utils/FirebaseAnalyticsHelper.kt
+++ app/src/fdroid/java/io/github/wulkanowy/utils/FirebaseAnalyticsHelper.kt
@@ -1,15 +1,13 @@
 package io.github.wulkanowy.utils
 
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FirebaseAnalyticsHelper @Inject constructor() {
 
+    @Suppress("UNUSED_PARAMETER")
     fun logEvent(name: String, vararg params: Pair<String, Any?>) {
-        Timber.d("Logging event (FE): $name, Bundle[{${params.joinToString(", ") {
-            it.first + "=" + it.second
-        }}}]")
+        // do nothing
     }
 }
--- app/src/fdroid/java/io/github/wulkanowy/utils/logger/LoggerHelper.kt
+++ app/src/fdroid/java/io/github/wulkanowy/utils/logger/LoggerHelper.kt
@@ -2,6 +2,7 @@ package io.github.wulkanowy.utils.logger
 
 import android.content.Context
 
+@Suppress("UNUSED_PARAMETER")
 fun initCrashlytics(context: Context) {
     // do nothing
 }
