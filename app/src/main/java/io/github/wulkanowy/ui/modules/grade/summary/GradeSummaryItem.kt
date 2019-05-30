11852312e5cebd5640f770ff0b6170aba65cd0b3
Mikołaj Pich <m.pich@outlook.com>
2019-05-31 01:14:07 +0200

    Add total points to grade summary

--- app/src/main/java/io/github/wulkanowy/ui/modules/grade/summary/GradeSummaryItem.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/grade/summary/GradeSummaryItem.kt
@@ -1,19 +1,20 @@
 package io.github.wulkanowy.ui.modules.grade.summary
 
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
 import eu.davidea.flexibleadapter.items.IFlexible
 import eu.davidea.viewholders.FlexibleViewHolder
 import io.github.wulkanowy.R
+import io.github.wulkanowy.data.db.entities.GradeSummary
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_grade_summary.*
 
 class GradeSummaryItem(
-    private val title: String,
-    private val average: String,
-    private val predictedGrade: String,
-    private val finalGrade: String
+    val summary: GradeSummary,
+    private val average: String
 ) : AbstractFlexibleItem<GradeSummaryItem.ViewHolder>() {
 
     override fun getLayoutRes() = R.layout.item_grade_summary
@@ -24,10 +25,13 @@ class GradeSummaryItem(
 
     override fun bindViewHolder(adapter: FlexibleAdapter<IFlexible<*>>, holder: ViewHolder, position: Int, payloads: MutableList<Any>?) {
         holder.run {
-            gradeSummaryItemTitle.text = title
+            gradeSummaryItemTitle.text = summary.subject
+            gradeSummaryItemPoints.text = summary.pointsSum
             gradeSummaryItemAverage.text = average
-            gradeSummaryItemPredicted.text = predictedGrade
-            gradeSummaryItemFinal.text = finalGrade
+            gradeSummaryItemPredicted.text = summary.predictedGrade
+            gradeSummaryItemFinal.text = summary.finalGrade
+
+            gradeSummaryItemPointsContainer.visibility = if (summary.pointsSum.isBlank()) GONE else VISIBLE
         }
     }
 
@@ -38,18 +42,20 @@ class GradeSummaryItem(
         other as GradeSummaryItem
 
         if (average != other.average) return false
-        if (title != other.title) return false
-        if (predictedGrade != other.predictedGrade) return false
-        if (finalGrade != other.finalGrade) return false
+        if (summary.subject != other.summary.subject) return false
+        if (summary.predictedGrade != other.summary.predictedGrade) return false
+        if (summary.finalGrade != other.summary.finalGrade) return false
+        if (summary.pointsSum != other.summary.pointsSum) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = title.hashCode()
+        var result = summary.subject.hashCode()
         result = 31 * result + average.hashCode()
-        result = 31 * result + predictedGrade.hashCode()
-        result = 31 * result + finalGrade.hashCode()
+        result = 31 * result + summary.predictedGrade.hashCode()
+        result = 31 * result + summary.finalGrade.hashCode()
+        result = 31 * result + summary.pointsSum.hashCode()
         return result
     }
 
