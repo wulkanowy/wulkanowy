8f2c5db4d9ed05039e37a441ad7a08d31b08d0fc
Dominik Korsa <dominik.korsa@gmail.com>
2019-09-07 13:06:42 +0200

    Rename ic_more_teachers.xml to ic_more_teacher.xml

--- app/src/main/java/io/github/wulkanowy/ui/modules/more/MoreFragment.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/more/MoreFragment.kt
@@ -56,7 +56,7 @@ class MoreFragment : BaseFragment(), MoreView, MainView.TitledView, MainView.Mai
         get() = context?.run { getString(R.string.mobile_devices_title) to getCompatDrawable(R.drawable.ic_more_mobile_devices) }
 
     override val teachersRes: Pair<String, Drawable?>?
-        get() = context?.run {  getString(R.string.teachers_title) to getCompatDrawable((R.drawable.ic_more_teachers)) }
+        get() = context?.run {  getString(R.string.teachers_title) to getCompatDrawable((R.drawable.ic_more_teacher)) }
 
     override val settingsRes: Pair<String, Drawable?>?
         get() = context?.run { getString(R.string.settings_title) to getCompatDrawable(R.drawable.ic_more_settings) }
