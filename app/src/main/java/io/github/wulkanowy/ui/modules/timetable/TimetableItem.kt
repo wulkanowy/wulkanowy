9b9542ea13a5e00045529b581ab47bf996e533a7
Dominik Korsa <dominik.korsa@gmail.com>
2019-09-21 20:41:22 +0200

    Seperate bindViewHolder function

--- app/src/main/java/io/github/wulkanowy/ui/modules/timetable/TimetableItem.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/timetable/TimetableItem.kt
@@ -1,13 +1,10 @@
 package io.github.wulkanowy.ui.modules.timetable
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.graphics.Paint
-import android.util.TypedValue
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import androidx.core.content.ContextCompat
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
 import eu.davidea.flexibleadapter.items.IFlexible
@@ -31,62 +28,108 @@ class TimetableItem(val lesson: Timetable, private val roomText: String) :
     @SuppressLint("SetTextI18n")
     override fun bindViewHolder(adapter: FlexibleAdapter<IFlexible<*>>, holder: ViewHolder, position: Int, payloads: MutableList<Any>?) {
         holder.apply {
+            updateFields(this, lesson)
+
+            timetableItemSubject.paintFlags =
+                if (lesson.canceled) timetableItemSubject.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                else timetableItemSubject.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+
+            updateDescription(this, lesson)
+
+            updateColors(this, lesson)
+        }
+    }
+
+    private fun updateFields(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
             timetableItemNumber.text = lesson.number.toString()
             timetableItemSubject.text = lesson.subject
             timetableItemRoom.text = if (lesson.room.isNotBlank()) lesson.room else ""
             timetableItemTeacher.text = if (lesson.teacher.isNotBlank()) lesson.teacher else ""
             timetableItemTimeStart.text = lesson.start.toFormattedString("HH:mm")
             timetableItemTimeFinish.text = lesson.end.toFormattedString("HH:mm")
-            timetableItemSubject.paintFlags =
-                if (lesson.canceled) timetableItemSubject.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                else timetableItemSubject.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
+    private fun updateDescription(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
             if (lesson.info.isNotBlank() && !lesson.changes) {
-                timetableItemDescription.visibility = VISIBLE
-                timetableItemDescription.text = lesson.info
-
-                timetableItemRoom.visibility = GONE
-                timetableItemTeacher.visibility = GONE
-
-                if (lesson.canceled) {
-                    timetableItemDescription.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorPrimary))
-                } else {
-                    timetableItemDescription.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
-                }
+                updateDescriptionNoChanges(holder, lesson)
             } else {
                 timetableItemDescription.visibility = GONE
 
                 timetableItemRoom.visibility = VISIBLE
                 timetableItemTeacher.visibility = VISIBLE
             }
+        }
+    }
 
+    private fun updateDescriptionNoChanges(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
+            timetableItemDescription.visibility = VISIBLE
+            timetableItemDescription.text = lesson.info
+
+            timetableItemRoom.visibility = GONE
+            timetableItemTeacher.visibility = GONE
+
+            if (lesson.canceled) {
+                timetableItemDescription.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorPrimary))
+            } else {
+                timetableItemDescription.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
+            }
+        }
+    }
+
+    private fun updateColors(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
             if (lesson.canceled) {
                 timetableItemNumber.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorPrimary))
                 timetableItemSubject.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorPrimary))
             } else {
-                if (lesson.changes || lesson.info.isNotBlank()) {
-                    timetableItemNumber.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
-                } else {
-                    timetableItemNumber.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorPrimary))
-                }
+                updateNumberColor(this, lesson)
+                updateSubjectColor(this, lesson)
+                updateRoomColor(this, lesson)
+                updateTeacherColor(this, lesson)
+            }
+        }
+    }
 
-                if (lesson.subjectOld.isNotBlank() && lesson.subjectOld != lesson.subject) {
-                    timetableItemSubject.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
-                } else {
-                    timetableItemSubject.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorPrimary))
-                }
+    private fun updateNumberColor(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
+            if (lesson.changes || lesson.info.isNotBlank()) {
+                timetableItemNumber.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
+            } else {
+                timetableItemNumber.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorPrimary))
+            }
+        }
+    }
 
-                if (lesson.roomOld.isNotBlank() && lesson.roomOld != lesson.room) {
-                    timetableItemRoom.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
-                } else {
-                    timetableItemRoom.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorSecondary))
-                }
+    private fun updateSubjectColor(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
+            if (lesson.subjectOld.isNotBlank() && lesson.subjectOld != lesson.subject) {
+                timetableItemSubject.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
+            } else {
+                timetableItemSubject.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorPrimary))
+            }
+        }
+    }
 
+    private fun updateRoomColor(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
+            if (lesson.roomOld.isNotBlank() && lesson.roomOld != lesson.room) {
+                timetableItemRoom.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
+            } else {
+                timetableItemRoom.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorSecondary))
+            }
+        }
+    }
 
-                if (lesson.teacherOld.isNotBlank() && lesson.teacherOld != lesson.teacher) {
-                    timetableItemTeacher.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
-                } else {
-                    timetableItemTeacher.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorPrimary))
-                }
+    private fun updateTeacherColor(holder: ViewHolder, lesson: Timetable) {
+        holder.apply {
+            if (lesson.teacherOld.isNotBlank() && lesson.teacherOld != lesson.teacher) {
+                timetableItemTeacher.setTextColor(holder.view.context.getThemeAttrColor(R.attr.colorTimetableChange))
+            } else {
+                timetableItemTeacher.setTextColor(holder.view.context.getThemeAttrColor(android.R.attr.textColorPrimary))
             }
         }
     }
