600108aa553147d97455a951722d1716d2effaa5
Dominik Korsa <dominik.korsa@gmail.com>
2019-09-21 13:10:41 +0200

    Add better timetable changes display

--- app/src/main/java/io/github/wulkanowy/ui/modules/timetable/TimetableItem.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/timetable/TimetableItem.kt
@@ -1,10 +1,13 @@
 package io.github.wulkanowy.ui.modules.timetable
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Paint
+import android.util.TypedValue
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import androidx.core.content.ContextCompat
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
 import eu.davidea.flexibleadapter.items.IFlexible
@@ -30,11 +33,60 @@ class TimetableItem(val lesson: Timetable, private val roomText: String) :
             timetableItemNumber.text = lesson.number.toString()
             timetableItemSubject.text = lesson.subject
             timetableItemRoom.text = if (lesson.room.isNotBlank()) "$roomText ${lesson.room}" else ""
-            timetableItemTime.text = "${lesson.start.toFormattedString("HH:mm")} - ${lesson.end.toFormattedString("HH:mm")}"
-            timetableItemAlert.visibility = if (lesson.changes || lesson.canceled) VISIBLE else GONE
+            timetableItemTeacher.text = if (lesson.teacher.isNotBlank()) lesson.teacher else ""
+            timetableItemTimeStart.text = lesson.start.toFormattedString("HH:mm")
+            timetableItemTimeFinish.text = lesson.end.toFormattedString("HH:mm")
             timetableItemSubject.paintFlags =
                 if (lesson.canceled) timetableItemSubject.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
                 else timetableItemSubject.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+            if (lesson.info.isNotBlank() && !lesson.changes) {
+                timetableItemDescription.visibility = VISIBLE
+                timetableItemDescription.text = lesson.info
+
+                timetableItemRoom.visibility = GONE
+                timetableItemTeacher.visibility = GONE
+
+                if (lesson.canceled) {
+                    timetableItemDescription.setTextColor(getColorByThemeAttr(holder.view.context, R.attr.colorPrimary))
+                } else {
+                    timetableItemDescription.setTextColor(ContextCompat.getColor(holder.view.context, R.color.timetable_change_color))
+                }
+            } else {
+                timetableItemDescription.visibility = GONE
+
+                timetableItemRoom.visibility = VISIBLE
+                timetableItemTeacher.visibility = VISIBLE
+            }
+
+            if (lesson.canceled) {
+                timetableItemNumber.setTextColor(getColorByThemeAttr(holder.view.context, R.attr.colorPrimary))
+                timetableItemSubject.setTextColor(getColorByThemeAttr(holder.view.context, R.attr.colorPrimary))
+            } else {
+                if (lesson.changes || lesson.info.isNotBlank()) {
+                    timetableItemNumber.setTextColor(ContextCompat.getColor(holder.view.context, R.color.timetable_change_color))
+                } else {
+                    timetableItemNumber.setTextColor(getPrimaryTextColor(holder.view.context))
+                }
+
+                if (lesson.subjectOld.isNotBlank() && lesson.subjectOld != lesson.subject) {
+                    timetableItemSubject.setTextColor(ContextCompat.getColor(holder.view.context, R.color.timetable_change_color))
+                } else {
+                    timetableItemSubject.setTextColor(getPrimaryTextColor(holder.view.context))
+                }
+
+                if (lesson.roomOld.isNotBlank() && lesson.roomOld != lesson.room) {
+                    timetableItemRoom.setTextColor(ContextCompat.getColor(holder.view.context, R.color.timetable_change_color))
+                } else {
+                    timetableItemRoom.setTextColor(getSecondaryTextColor(holder.view.context))
+                }
+
+
+                if (lesson.teacherOld.isNotBlank() && lesson.teacherOld != lesson.teacher) {
+                    timetableItemTeacher.setTextColor(ContextCompat.getColor(holder.view.context, R.color.timetable_change_color))
+                } else {
+                    timetableItemTeacher.setTextColor(getSecondaryTextColor(holder.view.context))
+                }
+            }
         }
     }
 
@@ -57,5 +109,28 @@ class TimetableItem(val lesson: Timetable, private val roomText: String) :
     class ViewHolder(val view: View, adapter: FlexibleAdapter<*>) : FlexibleViewHolder(view, adapter), LayoutContainer {
         override val containerView: View
             get() = contentView
+    }
+
+    private fun getColorByThemeAttr(context: Context, attr: Int): Int {
+        val typedValue = TypedValue()
+        val theme = context.theme
+        theme.resolveAttribute(attr, typedValue, true)
+        return typedValue.data
+    }
+
+    private fun getPrimaryTextColor(context: Context): Int {
+        val typedValue = TypedValue()
+        val theme = context.theme
+        theme.resolveAttribute(android.R.attr.textColorPrimary, typedValue, true)
+        val arr = context.theme.obtainStyledAttributes(typedValue.data, intArrayOf(android.R.attr.textColorPrimary))
+        return arr.getColor(0, -1)
+    }
+
+    private fun getSecondaryTextColor(context: Context): Int {
+        val typedValue = TypedValue()
+        val theme = context.theme
+        theme.resolveAttribute(android.R.attr.textColorSecondary, typedValue, true)
+        val arr = context.theme.obtainStyledAttributes(typedValue.data, intArrayOf(android.R.attr.textColorSecondary))
+        return arr.getColor(0, -1)
     }
 }
