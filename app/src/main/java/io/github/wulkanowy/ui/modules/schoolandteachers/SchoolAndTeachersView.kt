6136847a3684d78d836bfbadf0bb69d103dcb7dc
Mikołaj Pich <m.pich@outlook.com>
2019-10-20 21:21:21 +0200

    Add missing blank lines

--- app/src/main/java/io/github/wulkanowy/ui/modules/schoolandteachers/SchoolAndTeachersView.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/schoolandteachers/SchoolAndTeachersView.kt
@@ -5,8 +5,12 @@ import io.github.wulkanowy.ui.base.BaseView
 interface SchoolAndTeachersView : BaseView {
 
     val currentPageIndex: Int
+
     fun initView()
+
     fun showContent(show: Boolean)
+
     fun showProgress(show: Boolean)
+
     fun notifyChildLoadData(index: Int, forceRefresh: Boolean)
 }
