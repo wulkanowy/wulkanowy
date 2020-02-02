129cf3bc80b6d994b97e147c66d07edc6ae4c861
Mikołaj Pich <m.pich@outlook.com>
2020-02-02 20:09:34 +0100

    Update sdk

--- app/src/main/java/io/github/wulkanowy/data/repositories/recover/RecoverRemote.kt
+++ app/src/main/java/io/github/wulkanowy/data/repositories/recover/RecoverRemote.kt
@@ -2,18 +2,17 @@ package io.github.wulkanowy.data.repositories.recover
 
 import io.github.wulkanowy.sdk.Sdk
 import io.reactivex.Single
-import io.reactivex.observers.TestObserver
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RecoverRemote @Inject constructor(private val sdk: Sdk) {
+
     fun getRecaptchaSitekey(host: String, symbol: String): Single<Pair<String, String>> {
         return sdk.getPasswordResetCaptchaCode(host, symbol)
     }
 
-    fun sendRecoverRequest(url: String, symbol: String, email:String, recaptchaResponse: String) : Single<Pair<Boolean,String>>{
+    fun sendRecoverRequest(url: String, symbol: String, email:String, recaptchaResponse: String) : Single<String> {
         return sdk.sendPasswordResetRequest(url, symbol, email, recaptchaResponse)
     }
 }
--- app/src/main/java/io/github/wulkanowy/data/repositories/recover/RecoverRepository.kt
+++ app/src/main/java/io/github/wulkanowy/data/repositories/recover/RecoverRepository.kt
@@ -6,11 +6,12 @@ import javax.inject.Singleton
 
 @Singleton
 class RecoverRepository @Inject constructor(private val remote: RecoverRemote) {
+
     fun getRecaptchaSitekey(host: String, symbol: String) : Single<Pair<String, String>>{
         return remote.getRecaptchaSitekey(host, symbol)
     }
 
-    fun sendRecoverRequest(url: String, symbol: String, email:String, recaptchaResponse: String) : Single<Pair<Boolean,String>>{
+    fun sendRecoverRequest(url: String, symbol: String, email:String, recaptchaResponse: String) : Single<String> {
         return remote.sendRecoverRequest(url, symbol, email, recaptchaResponse)
     }
 }
