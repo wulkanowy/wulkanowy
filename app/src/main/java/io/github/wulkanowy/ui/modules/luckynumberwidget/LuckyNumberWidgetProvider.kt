3964eb87a8d5e9e64a05b3f0897a4b1963b179bd
Mikołaj Pich <m.pich@outlook.com>
2020-02-06 01:04:14 +0100

    Use AppWidgetProvider instead BroadcastReceiver

--- app/src/main/java/io/github/wulkanowy/ui/modules/luckynumberwidget/LuckyNumberWidgetProvider.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/luckynumberwidget/LuckyNumberWidgetProvider.kt
@@ -4,18 +4,13 @@ import android.annotation.TargetApi
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetManager.ACTION_APPWIDGET_DELETED
-import android.appwidget.AppWidgetManager.ACTION_APPWIDGET_OPTIONS_CHANGED
-import android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE
-import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID
-import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_IDS
-import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_OPTIONS
 import android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT
 import android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH
-import android.content.BroadcastReceiver
+import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.RemoteViews
@@ -34,7 +29,7 @@ import io.reactivex.Maybe
 import timber.log.Timber
 import javax.inject.Inject
 
-class LuckyNumberWidgetProvider : BroadcastReceiver() {
+class LuckyNumberWidgetProvider : AppWidgetProvider() {
 
     @Inject
     lateinit var studentRepository: StudentRepository
@@ -64,15 +59,12 @@ class LuckyNumberWidgetProvider : BroadcastReceiver() {
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onReceive(context: Context, intent: Intent) {
         AndroidInjection.inject(this, context)
-        when (intent.action) {
-            ACTION_APPWIDGET_UPDATE -> onUpdate(context, intent)
-            ACTION_APPWIDGET_DELETED -> onDelete(intent)
-            ACTION_APPWIDGET_OPTIONS_CHANGED -> onOptionsChange(context, intent)
-        }
+        super.onReceive(context, intent)
     }
 
-    private fun onUpdate(context: Context, intent: Intent) {
-        intent.getIntArrayExtra(EXTRA_APPWIDGET_IDS)?.forEach { appWidgetId ->
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        appWidgetIds?.forEach { appWidgetId ->
             val savedTheme = sharedPref.getLong(getThemeWidgetKey(appWidgetId), 0)
             val layoutId = if (savedTheme == 0L) R.layout.widget_luckynumber else R.layout.widget_luckynumber_dark
 
@@ -85,62 +77,32 @@ class LuckyNumberWidgetProvider : BroadcastReceiver() {
                 setOnClickPendingIntent(R.id.luckyNumberWidgetContainer, appIntent)
             }
 
-            setStyles(remoteView, intent)
+            setStyles(remoteView)
             appWidgetManager.updateAppWidget(appWidgetId, remoteView)
         }
     }
 
-    private fun onDelete(intent: Intent) {
-        val appWidgetId = intent.getIntExtra(EXTRA_APPWIDGET_ID, 0)
-        if (appWidgetId != 0) sharedPref.delete(getStudentWidgetKey(appWidgetId))
-    }
-
-    private fun getLuckyNumber(studentId: Long, appWidgetId: Int): LuckyNumber? {
-        return try {
-            studentRepository.isStudentSaved()
-                .filter { true }
-                .flatMap { studentRepository.getSavedStudents().toMaybe() }
-                .flatMap { students ->
-                    val student = students.singleOrNull { student -> student.id == studentId }
-                    when {
-                        student != null -> Maybe.just(student)
-                        studentId != 0L -> {
-                            studentRepository.isCurrentStudentSet()
-                                .filter { true }
-                                .flatMap { studentRepository.getCurrentStudent(false).toMaybe() }
-                                .doOnSuccess { sharedPref.putLong(getStudentWidgetKey(appWidgetId), it.id) }
-                        }
-                        else -> Maybe.empty()
-                    }
-                }
-                .flatMap { semesterRepository.getCurrentSemester(it).toMaybe() }
-                .flatMap { luckyNumberRepository.getLuckyNumber(it) }
-                .subscribeOn(schedulers.backgroundThread)
-                .blockingGet()
-        } catch (e: Exception) {
-            if (e.cause !is NoCurrentStudentException) {
-                Timber.e(e, "An error has occurred in lucky number provider")
-            }
-            null
+    override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
+        super.onDeleted(context, appWidgetIds)
+        appWidgetIds?.forEach { appWidgetId ->
+            if (appWidgetId != 0) sharedPref.delete(getStudentWidgetKey(appWidgetId))
         }
     }
 
-    private fun onOptionsChange(context: Context, intent: Intent) {
-        intent.extras?.getInt(EXTRA_APPWIDGET_ID)?.let { appWidgetId ->
-            val savedTheme = sharedPref.getLong(getThemeWidgetKey(appWidgetId), 0)
-            val layoutId = if (savedTheme == 0L) R.layout.widget_luckynumber else R.layout.widget_luckynumber_dark
+    override fun onAppWidgetOptionsChanged(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int, newOptions: Bundle?) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
 
-            val remoteView = RemoteViews(context.packageName, layoutId)
+        val savedTheme = sharedPref.getLong(getThemeWidgetKey(appWidgetId), 0)
+        val layoutId = if (savedTheme == 0L) R.layout.widget_luckynumber else R.layout.widget_luckynumber_dark
 
-            setStyles(remoteView, intent)
-            appWidgetManager.updateAppWidget(appWidgetId, remoteView)
-        }
+        val remoteView = RemoteViews(context.packageName, layoutId)
+
+        setStyles(remoteView, newOptions)
+        appWidgetManager.updateAppWidget(appWidgetId, remoteView)
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    private fun setStyles(views: RemoteViews, intent: Intent) {
-        val options = intent.extras?.getBundle(EXTRA_APPWIDGET_OPTIONS)
-
+    private fun setStyles(views: RemoteViews, options: Bundle? = null) {
         val maxWidth = options?.getInt(OPTION_APPWIDGET_MAX_WIDTH) ?: 150
         val maxHeight = options?.getInt(OPTION_APPWIDGET_MAX_HEIGHT) ?: 40
 
@@ -187,6 +149,36 @@ class LuckyNumberWidgetProvider : BroadcastReceiver() {
                     setViewVisibility(R.id.luckyNumberWidgetNumber, VISIBLE)
                 }
             }
+        }
+    }
+
+    private fun getLuckyNumber(studentId: Long, appWidgetId: Int): LuckyNumber? {
+        return try {
+            studentRepository.isStudentSaved()
+                .filter { true }
+                .flatMap { studentRepository.getSavedStudents().toMaybe() }
+                .flatMap { students ->
+                    val student = students.singleOrNull { student -> student.id == studentId }
+                    when {
+                        student != null -> Maybe.just(student)
+                        studentId != 0L -> {
+                            studentRepository.isCurrentStudentSet()
+                                .filter { true }
+                                .flatMap { studentRepository.getCurrentStudent(false).toMaybe() }
+                                .doOnSuccess { sharedPref.putLong(getStudentWidgetKey(appWidgetId), it.id) }
+                        }
+                        else -> Maybe.empty()
+                    }
+                }
+                .flatMap { semesterRepository.getCurrentSemester(it).toMaybe() }
+                .flatMap { luckyNumberRepository.getLuckyNumber(it) }
+                .subscribeOn(schedulers.backgroundThread)
+                .blockingGet()
+        } catch (e: Exception) {
+            if (e.cause !is NoCurrentStudentException) {
+                Timber.e(e, "An error has occurred in lucky number provider")
+            }
+            null
         }
     }
 }
