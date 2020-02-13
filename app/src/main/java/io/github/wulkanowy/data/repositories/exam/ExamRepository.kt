430ffa971ac790ba22378b6701d92050210342af
doteq <doteeqq@gmail.com>
2020-02-13 18:23:47 +0100

    Prevent duplicating events

--- app/src/main/java/io/github/wulkanowy/data/repositories/exam/ExamRepository.kt
+++ app/src/main/java/io/github/wulkanowy/data/repositories/exam/ExamRepository.kt
@@ -1,5 +1,16 @@
 package io.github.wulkanowy.data.repositories.exam
 
+import android.Manifest
+import android.R.attr.end
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Instances
+import androidx.core.content.ContextCompat
 import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
 import com.github.pwittchen.reactivenetwork.library.rx2.internet.observing.InternetObservingSettings
 import io.github.wulkanowy.data.db.entities.Exam
@@ -12,7 +23,10 @@ import io.reactivex.Completable
 import io.reactivex.Single
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDate.now
+import org.threeten.bp.ZoneOffset
+import timber.log.Timber
 import java.net.UnknownHostException
+import java.util.TimeZone
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -21,7 +35,8 @@ class ExamRepository @Inject constructor(
     private val settings: InternetObservingSettings,
     private val local: ExamLocal,
     private val remote: ExamRemote,
-    private val preferencesRepository: PreferencesRepository
+    private val preferencesRepository: PreferencesRepository,
+    private val context: Context
 ) {
 
     fun getExams(semester: Semester, startDate: LocalDate, endDate: LocalDate, forceRefresh: Boolean = false): Single<List<Exam>> {
@@ -54,5 +69,43 @@ class ExamRepository @Inject constructor(
 
     fun updateExams(exams: List<Exam>): Completable {
         return Completable.fromCallable { local.updateExams(exams) }
+    }
+
+    fun createCalendarEvents(exams: List<Exam>) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR)
+            === PackageManager.PERMISSION_GRANTED) {
+            exams.onEach {
+                if (!isEventExists(
+                        "${it.subject} - ${it.type}",
+                        now().atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli().toString(),
+                        now().atStartOfDay().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli().toString()
+                    )) {
+                    val cr: ContentResolver = context.contentResolver
+                    val event = ContentValues()
+                    Timber.i(now().atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli().toString())
+                    event.apply {
+                        put("calendar_id", preferencesRepository.calendarSyncId)
+                        put("title", "${it.subject} - ${it.type}")
+                        put("description", "${it.description}\n\nWygenerowano przez Wulkanowy Dzienniczek")
+                        put("allDay", 1)
+                        put("dtstart", it.date.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli())
+                        put("duration", "PT1D")
+                        put("eventTimezone", TimeZone.getTimeZone("GMT").id)
+                        put("hasAlarm", 0)
+                    }
+                    cr.insert(CalendarContract.Events.CONTENT_URI, event)
+                }
+            }
+        }
+    }
+
+    fun isEventExists(name: String, dtstart: String, dtend: String): Boolean{
+        val fields = arrayOf(
+            Instances._ID,
+            Instances.BEGIN,
+            Instances.END,
+            Instances.EVENT_ID)
+        val cursor: Cursor = Instances.query(context.contentResolver, fields, dtstart.toLong(), dtend.toLong(), name)
+        return cursor.count > 0
     }
 }
--- app/src/main/java/io/github/wulkanowy/services/sync/works/ExamWork.kt
+++ app/src/main/java/io/github/wulkanowy/services/sync/works/ExamWork.kt
@@ -1,56 +1,24 @@
 package io.github.wulkanowy.services.sync.works
 
-import android.annotation.SuppressLint
-import android.content.ContentResolver
-import android.content.ContentValues
-import android.content.Context
-import android.net.Uri
-import android.provider.CalendarContract
-import io.github.wulkanowy.data.db.entities.Exam
 import io.github.wulkanowy.data.db.entities.Semester
 import io.github.wulkanowy.data.db.entities.Student
 import io.github.wulkanowy.data.repositories.exam.ExamRepository
-import io.github.wulkanowy.data.repositories.preferences.PreferencesRepository
 import io.github.wulkanowy.utils.friday
 import io.github.wulkanowy.utils.monday
 import io.reactivex.Completable
 import org.threeten.bp.LocalDate.now
-import org.threeten.bp.ZoneOffset
-import timber.log.Timber
-import java.util.TimeZone
 import javax.inject.Inject
 
 class ExamWork @Inject constructor(
-    private val examRepository: ExamRepository,
-    private val context: Context,
-    private val preferencesRepository: PreferencesRepository): Work {
+    private val examRepository: ExamRepository
+) : Work {
 
     override fun create(student: Student, semester: Semester): Completable {
         return examRepository.getExams(semester, now().monday, now().friday, true)
             .flatMap { examRepository.getNotCalendarSyncedExams(semester) }
             .flatMapCompletable {
-                if(it.isNotEmpty()) calendarSync(it)
+                if (it.isNotEmpty()) examRepository.createCalendarEvents(it)
                 examRepository.updateExams(it.onEach { exam -> exam.calendarSync = true })
             }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun calendarSync( exams: List<Exam> ){
-        exams.onEach {
-            val cr: ContentResolver = context.contentResolver
-            val event = ContentValues()
-            Timber.i(now().atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli().toString())
-            event.apply {
-                put("calendar_id", preferencesRepository.calendarSyncId);
-                put("title", "${it.subject} - ${it.type}")
-                put("description", "${it.description}\n\nWygenerowano przez Wulkanowy Dzienniczek")
-                put("allDay", 1)
-                put("dtstart", it.date.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli())
-                put("duration", "PT1D");
-                put("eventTimezone", TimeZone.getTimeZone("GMT").id)
-                put("hasAlarm", 0);
-            }
-            val uri: Uri? = cr.insert(CalendarContract.Events.CONTENT_URI, event)
-        }
     }
 }
