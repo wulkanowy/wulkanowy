d24e680839b8df6dfacc1199881e2d3d31dabfb4
Mikołaj Pich <m.pich@outlook.com>
2020-04-02 21:04:33 +0200

    Fix migration from develop

--- app/src/main/java/io/github/wulkanowy/data/db/migrations/Migration25.kt
+++ app/src/main/java/io/github/wulkanowy/data/db/migrations/Migration25.kt
@@ -7,6 +7,6 @@ class Migration25 : Migration(24, 25) {
 
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE Homework ADD COLUMN is_done INTEGER NOT NULL DEFAULT 0")
-        database.execSQL("ALTER TABLE Homework ADD COLUMN attachments TEXT NOT NULL DEFAULT \"\"")
+        database.execSQL("ALTER TABLE Homework ADD COLUMN attachments TEXT NOT NULL DEFAULT \"[]\"")
     }
 }
