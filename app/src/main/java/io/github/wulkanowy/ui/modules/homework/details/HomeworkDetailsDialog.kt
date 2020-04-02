9079e86550187a03c77ace48b62e79c9ce2f8262
Mikołaj Pich <m.pich@outlook.com>
2020-04-03 00:07:03 +0200

    Add attachments header to dialog

--- app/src/main/java/io/github/wulkanowy/ui/modules/homework/details/HomeworkDetailsDialog.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/homework/details/HomeworkDetailsDialog.kt
@@ -56,9 +56,12 @@ class HomeworkDetailsDialog : BaseDialogFragment(), HomeworkDetailsView {
         homeworkDialogSubject.text = homework.subject
         homeworkDialogTeacher.text = homework.teacher
         homeworkDialogContent.movementMethod = LinkMovementMethod.getInstance()
-        homeworkDialogContent.text = HtmlCompat.fromHtml(homework.content + "<br><br>" +
-            homework.attachments.joinToString("<br>") { "<a href='${it.first}'>${it.second}</a>" },
-            HtmlCompat.FROM_HTML_MODE_COMPACT)
+        homeworkDialogContent.text = homework.content
+        homeworkDialogAttachments.movementMethod = LinkMovementMethod.getInstance()
+        homeworkDialogAttachments.text = HtmlCompat.fromHtml(homework.attachments.joinToString("<br>") {
+            "<a href='${it.first}'>${it.second}</a>"
+        }, HtmlCompat.FROM_HTML_MODE_COMPACT).ifBlank { getString(R.string.all_no_data) }
+
         homeworkDialogRead.text = view?.context?.getString(if (homework.isDone) R.string.homework_mark_as_undone else R.string.homework_mark_as_done)
         homeworkDialogRead.setOnClickListener { presenter.toggleDone(homework) }
         homeworkDialogClose.setOnClickListener { dismiss() }
