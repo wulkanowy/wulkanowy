0ae6f43e3e266f459edc6a633c8e0be2dedac3c1
Mikołaj Pich <m.pich@outlook.com>
2020-04-10 22:26:20 +0200

    Add Sdk.init() in all repositories

---
+++ app/src/main/java/io/github/wulkanowy/utils/SdkExtension.kt
@@ -0,0 +1,26 @@
+package io.github.wulkanowy.utils
+
+import io.github.wulkanowy.data.db.entities.Student
+import io.github.wulkanowy.sdk.Sdk
+
+fun Sdk.init(student: Student): Sdk {
+    email = student.email
+    password = student.password
+    symbol = student.symbol
+    schoolSymbol = student.schoolSymbol
+    studentId = student.studentId
+    classId = student.classId
+
+    if (Sdk.Mode.valueOf(student.loginMode) != Sdk.Mode.API) {
+        scrapperBaseUrl = student.scrapperBaseUrl
+        loginType = Sdk.ScrapperLoginType.valueOf(student.loginType)
+    }
+    loginId = student.userLoginId
+
+    mode = Sdk.Mode.valueOf(student.loginMode)
+    mobileBaseUrl = student.mobileBaseUrl
+    certKey = student.certificateKey
+    privateKey = student.privateKey
+
+    return this
+}
