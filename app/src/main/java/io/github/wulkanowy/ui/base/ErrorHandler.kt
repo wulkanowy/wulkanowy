391b2d0d8737a41749fd40807016ee46b8c44b1a
Mikołaj Pich <m.pich@outlook.com>
2020-04-19 17:19:09 +0200

    Remove unnecessary errors match in ErrorHandler::proceed

--- app/src/main/java/io/github/wulkanowy/ui/base/ErrorHandler.kt
+++ app/src/main/java/io/github/wulkanowy/ui/base/ErrorHandler.kt
@@ -4,15 +4,9 @@ import android.content.res.Resources
 import com.chuckerteam.chucker.api.ChuckerCollector
 import io.github.wulkanowy.data.exceptions.NoCurrentStudentException
 import io.github.wulkanowy.sdk.exception.BadCredentialsException
-import io.github.wulkanowy.sdk.exception.FeatureDisabledException
-import io.github.wulkanowy.sdk.exception.FeatureNotAvailableException
-import io.github.wulkanowy.sdk.exception.NotLoggedInException
-import io.github.wulkanowy.sdk.exception.ServiceUnavailableException
 import io.github.wulkanowy.utils.getString
 import io.github.wulkanowy.utils.security.ScramblerException
 import timber.log.Timber
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
 import javax.inject.Inject
 
 open class ErrorHandler @Inject constructor(protected val resources: Resources, private val chuckerCollector: ChuckerCollector) {
@@ -30,17 +24,10 @@ open class ErrorHandler @Inject constructor(protected val resources: Resources, 
     }
 
     protected open fun proceed(error: Throwable) {
-        val message = resources.getString(error)
         when (error) {
-            is UnknownHostException -> showErrorMessage(message, error)
-            is SocketTimeoutException -> showErrorMessage(message, error)
-            is NotLoggedInException -> showErrorMessage(message, error)
-            is ServiceUnavailableException -> showErrorMessage(message, error)
-            is FeatureDisabledException -> showErrorMessage(message, error)
             is ScramblerException, is BadCredentialsException -> onSessionExpired()
             is NoCurrentStudentException -> onNoCurrentStudent()
-            is FeatureNotAvailableException -> showErrorMessage(message, error)
-            else -> showErrorMessage(message, error)
+            else -> showErrorMessage(resources.getString(error), error)
         }
     }
 
