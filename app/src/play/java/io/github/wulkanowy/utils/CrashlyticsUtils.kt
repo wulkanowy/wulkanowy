bb3c55babe4956bf5bd1a5cfbdc6f585c515b1bd
Mikołaj Pich <m.pich@outlook.com>
2020-05-01 16:23:34 +0200

    Use firebase crashlytics in crashlytics trees

--- app/src/play/java/io/github/wulkanowy/utils/CrashlyticsUtils.kt
+++ app/src/play/java/io/github/wulkanowy/utils/CrashlyticsUtils.kt
@@ -1,19 +1,46 @@
 package io.github.wulkanowy.utils
 
 import android.util.Log
-import fr.bipi.tressence.crash.CrashlyticsLogExceptionTree
-import fr.bipi.tressence.crash.CrashlyticsLogTree
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import fr.bipi.tressence.base.FormatterPriorityTree
+import fr.bipi.tressence.common.StackTraceRecorder
 import io.github.wulkanowy.sdk.exception.FeatureDisabledException
 import io.github.wulkanowy.sdk.exception.FeatureNotAvailableException
 import java.net.UnknownHostException
 
-class CrashlyticsTree : CrashlyticsLogTree(Log.VERBOSE) {
+class CrashlyticsTree : FormatterPriorityTree(Log.VERBOSE) {
+
+    private val crashlytics by lazy { FirebaseCrashlytics.getInstance() }
+
+    override fun skipLog(priority: Int, tag: String?, message: String, t: Throwable?): Boolean {
+        if (t is FeatureDisabledException || t is FeatureNotAvailableException || t is UnknownHostException) {
+            return true
+        }
+
+        return super.skipLog(priority, tag, message, t)
+    }
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        if (t is FeatureDisabledException || t is FeatureNotAvailableException || t is UnknownHostException) return
+        if (skipLog(priority, tag, message, t)) return
 
-        super.log(priority, tag, message, t)
+        crashlytics.log(format(priority, tag, message))
     }
 }
 
-class CrashlyticsExceptionTree : CrashlyticsLogExceptionTree()
+class CrashlyticsExceptionTree : FormatterPriorityTree(Log.ERROR) {
+
+    private val crashlytics by lazy { FirebaseCrashlytics.getInstance() }
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        if (skipLog(priority, tag, message, t)) return
+
+        crashlytics.setCustomKey("priority", priority)
+        crashlytics.setCustomKey("tag", tag.orEmpty())
+        crashlytics.setCustomKey("message", message)
+        if (t != null) {
+            crashlytics.recordException(t)
+        } else {
+            crashlytics.recordException(StackTraceRecorder(format(priority, tag, message)))
+        }
+    }
+}
