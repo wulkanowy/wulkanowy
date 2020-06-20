c160d42dc76352d6fbaeabbb67d89b6674a0a9d8
Mikołaj Pich <m.pich@outlook.com>
2020-06-20 14:50:26 +0200

    Add dispatchers provider

--- app/src/main/java/io/github/wulkanowy/data/repositories/appcreator/AppCreatorRepository.kt
+++ app/src/main/java/io/github/wulkanowy/data/repositories/appcreator/AppCreatorRepository.kt
@@ -3,16 +3,19 @@ package io.github.wulkanowy.data.repositories.appcreator
 import android.content.res.AssetManager
 import com.google.gson.Gson
 import io.github.wulkanowy.data.pojos.Contributor
-import kotlinx.coroutines.Dispatchers
+import io.github.wulkanowy.utils.DispatchersProvider
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AppCreatorRepository @Inject constructor(private val assets: AssetManager) {
+class AppCreatorRepository @Inject constructor(
+    private val assets: AssetManager,
+    private val dispatchers: DispatchersProvider
+) {
 
     suspend fun getAppCreators(): List<Contributor> {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatchers.backgroundThread) {
             Gson().fromJson(
                 assets.open("contributors.json").bufferedReader().use { it.readText() },
                 Array<Contributor>::class.java
--- app/src/main/java/io/github/wulkanowy/data/repositories/logger/LoggerRepository.kt
+++ app/src/main/java/io/github/wulkanowy/data/repositories/logger/LoggerRepository.kt
@@ -1,20 +1,23 @@
 package io.github.wulkanowy.data.repositories.logger
 
 import android.content.Context
-import kotlinx.coroutines.Dispatchers
+import io.github.wulkanowy.utils.DispatchersProvider
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileNotFoundException
 import javax.inject.Inject
 
-class LoggerRepository @Inject constructor(private val context: Context) {
+class LoggerRepository @Inject constructor(
+    private val context: Context,
+    private val dispatchers: DispatchersProvider
+) {
 
     suspend fun getLastLogLines(): List<String> {
         return getLastModified().readText().split("\n")
     }
 
     suspend fun getLogFiles(): List<File> {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatchers.backgroundThread) {
             File(context.filesDir.absolutePath).listFiles(File::isFile)?.filter {
                 it.name.endsWith(".log")
             }!!
@@ -22,7 +25,7 @@ class LoggerRepository @Inject constructor(private val context: Context) {
     }
 
     private suspend fun getLastModified(): File {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatchers.backgroundThread) {
             var lastModifiedTime = Long.MIN_VALUE
             var chosenFile: File? = null
             File(context.filesDir.absolutePath).listFiles(File::isFile)?.forEach { file ->
