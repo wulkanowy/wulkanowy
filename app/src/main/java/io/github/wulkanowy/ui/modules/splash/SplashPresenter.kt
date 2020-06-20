520b18a3148bed8265e2b6f048ccdee84266a41c
Mikołaj Pich <m.pich@outlook.com>
2020-06-20 23:45:42 +0200

    Refactor data loading in splash screen

--- app/src/main/java/io/github/wulkanowy/ui/modules/splash/SplashPresenter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/splash/SplashPresenter.kt
@@ -4,7 +4,10 @@ import io.github.wulkanowy.data.repositories.student.StudentRepository
 import io.github.wulkanowy.ui.base.BasePresenter
 import io.github.wulkanowy.ui.base.ErrorHandler
 import io.github.wulkanowy.utils.SchedulersProvider
-import kotlinx.coroutines.rx2.rxSingle
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SplashPresenter @Inject constructor(
@@ -15,14 +18,15 @@ class SplashPresenter @Inject constructor(
 
     override fun onAttachView(view: SplashView) {
         super.onAttachView(view)
-        disposable.add(rxSingle { studentRepository.isCurrentStudentSet() }
-            .subscribeOn(schedulers.backgroundThread)
-            .observeOn(schedulers.mainThread)
-            .subscribe({
-                view.apply {
-                    if (it) openMainView()
-                    else openLoginView()
+        launch {
+            flowOf(studentRepository.isCurrentStudentSet())
+                .catch { errorHandler.dispatch(it) }
+                .collect {
+                    view.apply {
+                        if (it) openMainView()
+                        else openLoginView()
+                    }
                 }
-            }, { errorHandler.dispatch(it) }))
+        }
     }
 }
