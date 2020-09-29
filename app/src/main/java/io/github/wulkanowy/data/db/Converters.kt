12a96f9db41ce8c18e13dbdcd18dfe2463eeec3a
Mikołaj Pich <m.pich@outlook.com>
2020-09-29 11:10:37 +0200

    Add custom list pair adapter

--- app/src/main/java/io/github/wulkanowy/data/db/Converters.kt
+++ app/src/main/java/io/github/wulkanowy/data/db/Converters.kt
@@ -3,7 +3,7 @@ package io.github.wulkanowy.data.db
 import androidx.room.TypeConverter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import timber.log.Timber
+import io.github.wulkanowy.data.db.adapters.PairAdapterFactory
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -13,14 +13,14 @@ import java.util.Date
 
 class Converters {
 
-    private val moshi by lazy { Moshi.Builder().build() }
+    private val moshi by lazy { Moshi.Builder().add(PairAdapterFactory).build() }
 
     private val integerListAdapter by lazy {
         moshi.adapter<List<Int>>(Types.newParameterizedType(List::class.java, Integer::class.java))
     }
 
-    private val stringListMapAdapter by lazy {
-        moshi.adapter<List<Map<String, String>>>(Types.newParameterizedType(List::class.java, Map::class.java, String::class.java))
+    private val stringListPairAdapter by lazy {
+        moshi.adapter<List<Pair<String, String>>>(Types.newParameterizedType(List::class.java, Pair::class.java, String::class.java, String::class.java))
     }
 
     @TypeConverter
@@ -61,16 +61,11 @@ class Converters {
 
     @TypeConverter
     fun stringPairListToJson(list: List<Pair<String, String>>): String {
-        return stringListMapAdapter.toJson(list.map { mapOf(it) })
+        return stringListPairAdapter.toJson(list)
     }
 
     @TypeConverter
     fun jsonToStringPairList(value: String): List<Pair<String, String>> {
-        if (value.startsWith("{")) {
-            Timber.w("Malformed json: Expected BEGIN_ARRAY but was BEGIN_OBJECT at path \$")
-            return emptyList()
-        }
-
-        return stringListMapAdapter.fromJson(value)?.flatMap { map -> map.map { it.key to it.value } }.orEmpty()
+        return stringListPairAdapter.fromJson(value).orEmpty()
     }
 }
