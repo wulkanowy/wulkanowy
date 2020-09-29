70ec02a45274341d8cd8d9fe7aa830cd24f7895d
Mikołaj Pich <m.pich@outlook.com>
2020-09-29 20:40:39 +0200

    Update sdk

--- app/src/test/java/io/github/wulkanowy/data/db/ConvertersTest.kt
+++ app/src/test/java/io/github/wulkanowy/data/db/ConvertersTest.kt
@@ -15,8 +15,8 @@ class ConvertersTest {
 
     @Test
     fun jsonToStringPairList() {
-        assertEquals(Converters().jsonToStringPairList("[{\"first\":\"aaa\",\"second\":\"bbb\"},{\"first\":\"ccc\",\"second\":\"ddd\"}]"), listOf("aaa" to "bbb", "ccc" to "ddd"))
         assertEquals(Converters().jsonToStringPairList("[{\"first\":\"aaa\",\"second\":\"bbb\"},{\"first\":\"ccc\",\"second\":\"ddd\"},{\"first\":\"eee\",\"second\":\"fff\"}]"), listOf("aaa" to "bbb", "ccc" to "ddd", "eee" to "fff"))
+        assertEquals(Converters().jsonToStringPairList("[{\"first\":\"aaa\",\"second\":\"bbb\"},{\"first\":\"ccc\",\"second\":\"ddd\"}]"), listOf("aaa" to "bbb", "ccc" to "ddd"))
         assertEquals(Converters().jsonToStringPairList("[{\"first\":\"aaa\",\"second\":\"bbb\"}]"), listOf("aaa" to "bbb"))
         assertEquals(Converters().jsonToStringPairList("[]"), listOf<Pair<String, String>>())
     }
