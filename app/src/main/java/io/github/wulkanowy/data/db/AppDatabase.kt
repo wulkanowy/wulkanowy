6b7e4798e6ab359c593d6bbd7d1e72daaae5624d
Faierbel <RafalBO99@outlook.com>
2021-01-24 18:31:05 +0100

    Fix migration test

--- app/src/main/java/io/github/wulkanowy/data/db/AppDatabase.kt
+++ app/src/main/java/io/github/wulkanowy/data/db/AppDatabase.kt
@@ -82,6 +82,7 @@ import io.github.wulkanowy.data.db.migrations.Migration28
 import io.github.wulkanowy.data.db.migrations.Migration29
 import io.github.wulkanowy.data.db.migrations.Migration3
 import io.github.wulkanowy.data.db.migrations.Migration30
+import io.github.wulkanowy.data.db.migrations.Migration31
 import io.github.wulkanowy.data.db.migrations.Migration4
 import io.github.wulkanowy.data.db.migrations.Migration5
 import io.github.wulkanowy.data.db.migrations.Migration6
@@ -127,7 +128,7 @@ import javax.inject.Singleton
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
-        const val VERSION_SCHEMA = 30
+        const val VERSION_SCHEMA = 31
 
         fun getMigrations(sharedPrefProvider: SharedPrefProvider): Array<Migration> {
             return arrayOf(
@@ -159,7 +160,8 @@ abstract class AppDatabase : RoomDatabase() {
                 Migration27(),
                 Migration28(),
                 Migration29(),
-                Migration30()
+                Migration30(),
+                Migration31()
             )
         }
 
