7851302ff39d8b61b4bedc5c859a0755b04e396e
Daxxxis <czupryn845@gmail.com>
2021-02-22 14:00:21 +0100

    Cleanup for Advanced Settings

--- app/src/main/java/io/github/wulkanowy/ui/modules/settings/advanced/AdvancedView.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/settings/advanced/AdvancedView.kt
@@ -4,21 +4,7 @@ import io.github.wulkanowy.ui.base.BaseView
 
 interface AdvancedView : BaseView {
 
-    val syncSuccessString: String
-
-    val syncFailedString: String
-
     fun initView()
 
     fun recreateView()
-
-    fun updateLanguage(langCode: String)
-
-    fun updateLanguageToFollowSystem()
-
-    fun setServicesSuspended(serviceEnablesKey: String, isHolidays: Boolean)
-
-    fun setSyncInProgress(inProgress: Boolean)
-
-    fun showFixSyncDialog()
 }
