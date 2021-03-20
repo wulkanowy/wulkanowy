5588824f1b3d1b67173b0495ceae5abe541c039f
Rafał Borcz <RafalBO99@outlook.com>
2021-03-20 15:17:36 +0100

    Remove unused resources

--- app/src/main/java/io/github/wulkanowy/ui/modules/timetablewidget/TimetableWidgetConfigureActivity.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/timetablewidget/TimetableWidgetConfigureActivity.kt
@@ -37,13 +37,19 @@ class TimetableWidgetConfigureActivity :
 
     private var dialog: AlertDialog? = null
 
-    override public fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setResult(RESULT_CANCELED)
-        setContentView(ActivityWidgetConfigureBinding.inflate(layoutInflater).apply { binding = this }.root)
+        setContentView(
+            ActivityWidgetConfigureBinding.inflate(layoutInflater).apply { binding = this }.root
+        )
 
         intent.extras.let {
-            presenter.onAttachView(this, it?.getInt(EXTRA_APPWIDGET_ID), it?.getBoolean(EXTRA_FROM_PROVIDER))
+            presenter.onAttachView(
+                this,
+                it?.getInt(EXTRA_APPWIDGET_ID),
+                it?.getBoolean(EXTRA_FROM_PROVIDER)
+            )
         }
     }
 
