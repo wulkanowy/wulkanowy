814ea27826ad22e09bbdf52d8ef42f17b34900dc
Rafał Borcz <RafalBO99@outlook.com>
2021-04-18 15:45:05 +0200

    Remove unused resource and use solid drawable instead of a gradient

--- app/src/main/java/io/github/wulkanowy/ui/widgets/DividerItemDecoration.kt
+++ app/src/main/java/io/github/wulkanowy/ui/widgets/DividerItemDecoration.kt
@@ -2,29 +2,24 @@ package io.github.wulkanowy.ui.widgets
 
 import android.content.Context
 import android.graphics.Canvas
-import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import io.github.wulkanowy.R
 
 class DividerItemDecoration(context: Context) : DividerItemDecoration(context, VERTICAL) {
-
-    private val mDivider = ContextCompat.getDrawable(context, R.drawable.item_divider)
 
     override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         canvas.save()
         val dividerLeft = parent.paddingLeft
         val dividerRight = parent.width - parent.paddingRight
-        val childCount = parent.childCount
 
-        for (i in 0..childCount - 2) {
-            val child: View = parent.getChildAt(i)
+        for (i in 0..parent.childCount - 2) {
+            val child = parent.getChildAt(i)
             val params = child.layoutParams as RecyclerView.LayoutParams
-            val dividerTop: Int = child.bottom + params.bottomMargin
-            val dividerBottom = dividerTop + mDivider!!.intrinsicHeight
-            mDivider.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom)
-            mDivider.draw(canvas)
+            val dividerTop = child.bottom + params.bottomMargin
+            val dividerBottom = dividerTop + drawable!!.intrinsicHeight
+
+            drawable?.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom)
+            drawable?.draw(canvas)
         }
         canvas.restore()
     }
