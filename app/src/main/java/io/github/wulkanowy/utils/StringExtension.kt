0603f7cf344629782dcc640e6517a5874ec20efc
Rafał Borcz <RafalBO99@outlook.com>
2021-05-06 00:25:27 +0200

    Update AGP

--- app/src/main/java/io/github/wulkanowy/utils/StringExtension.kt
+++ app/src/main/java/io/github/wulkanowy/utils/StringExtension.kt
@@ -2,9 +2,8 @@ package io.github.wulkanowy.utils
 
 import java.util.Locale
 
-inline fun String?.ifNullOrBlank(defaultValue: () -> String) = if (isNullOrBlank()) {
-    defaultValue()
-} else this
+inline fun String?.ifNullOrBlank(defaultValue: () -> String) =
+    if (isNullOrBlank()) defaultValue() else this
 
 fun String.capitalise() = replaceFirstChar {
     if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
