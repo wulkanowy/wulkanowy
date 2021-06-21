d2cad16d8f7c4ebce01ac5db74c6e8958fb146a7
Mikołaj Pich <m.pich@outlook.com>
2021-06-21 09:50:28 +0200

    Add more mock data to notification debug

--- app/src/main/java/io/github/wulkanowy/ui/modules/debug/notification/mock/gradeSummary.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/debug/notification/mock/gradeSummary.kt
@@ -3,16 +3,28 @@ package io.github.wulkanowy.ui.modules.debug.notification.mock
 import io.github.wulkanowy.data.db.entities.GradeSummary
 
 val debugGradeSummaryItems = listOf(
-    GradeSummary(
-        semesterId = 0,
-        studentId = 0,
-        position = 0,
-        subject = "",
-        predictedGrade = "",
-        finalGrade = "",
-        proposedPoints = "",
-        finalPoints = "",
-        pointsSum = "",
-        average = .0
-    ),
+    generateSummary("Matematyka", "2-", "2"),
+    generateSummary("Fizyka", "1", "2"),
+    generateSummary("Geografia", "4+", "5"),
+    generateSummary("Sieci komputerowe", "2", "5"),
+    generateSummary("Systemy operacyjne", "3", "4"),
+    generateSummary("Język polski", "1", "3"),
+    generateSummary("Język angielski", "4", "3"),
+    generateSummary("Religia", "5", "6"),
+    generateSummary("Język niemiecki", "2", "2"),
+    generateSummary("Wychowanie fizyczne", "5", "5"),
+    generateSummary("Biologia", "4", "4"),
+)
+
+private fun generateSummary(subject: String, predicted: String, final: String) = GradeSummary(
+    semesterId = 0,
+    studentId = 0,
+    position = 0,
+    subject = subject,
+    predictedGrade = predicted,
+    finalGrade = final,
+    proposedPoints = "",
+    finalPoints = "",
+    pointsSum = "",
+    average = .0
 )
--- app/src/main/java/io/github/wulkanowy/ui/modules/debug/notification/mock/luckyNumber.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/debug/notification/mock/luckyNumber.kt
@@ -2,9 +2,11 @@ package io.github.wulkanowy.ui.modules.debug.notification.mock
 
 import io.github.wulkanowy.data.db.entities.LuckyNumber
 import java.time.LocalDate
+import kotlin.random.Random
 
-val debugLuckyNumber = LuckyNumber(
-    studentId = 0,
-    date = LocalDate.now(),
-    luckyNumber = 0,
-)
+val debugLuckyNumber
+    get() = LuckyNumber(
+        studentId = 0,
+        date = LocalDate.now(),
+        luckyNumber = Random.nextInt(1, 128),
+    )
