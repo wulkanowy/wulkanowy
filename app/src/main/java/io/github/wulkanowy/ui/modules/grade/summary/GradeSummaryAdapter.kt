8df00c6a3534ebac30b237fba6d1732094f735e0
Rafał Borcz <RafalBO99@outlook.com>
2021-08-23 15:34:46 +0200

    Change string and size

--- app/src/main/java/io/github/wulkanowy/ui/modules/grade/summary/GradeSummaryAdapter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/grade/summary/GradeSummaryAdapter.kt
@@ -36,8 +36,12 @@ class GradeSummaryAdapter @Inject constructor(
         val inflater = LayoutInflater.from(parent.context)
 
         return when (viewType) {
-            ViewType.HEADER.id -> HeaderViewHolder(ScrollableHeaderGradeSummaryBinding.inflate(inflater, parent, false))
-            ViewType.ITEM.id -> ItemViewHolder(ItemGradeSummaryBinding.inflate(inflater, parent, false))
+            ViewType.HEADER.id -> HeaderViewHolder(
+                ScrollableHeaderGradeSummaryBinding.inflate(inflater, parent, false)
+            )
+            ViewType.ITEM.id -> ItemViewHolder(
+                ItemGradeSummaryBinding.inflate(inflater, parent, false)
+            )
             else -> throw IllegalStateException()
         }
     }
@@ -52,18 +56,28 @@ class GradeSummaryAdapter @Inject constructor(
     private fun bindHeaderViewHolder(binding: ScrollableHeaderGradeSummaryBinding) {
         if (items.isEmpty()) return
 
+        val context = binding.root.context
+        val finalItemsCount = items.count { it.finalGrade.matches("[0-6][+-]?".toRegex()) }
+        val calculatedItemsCount = items.count { value -> value.average != 0.0 }
+        val finalAverage = items.calcAverage(
+            preferencesRepository.gradePlusModifier,
+            preferencesRepository.gradeMinusModifier
+        )
+        val calculatedAverage = items.filter { value -> value.average != 0.0 }
+            .map { values -> values.average }
+            .reversed() // fix average precision
+            .average()
+
         with(binding) {
-            gradeSummaryScrollableHeaderFinal.text = formatAverage(items.calcAverage(preferencesRepository.gradePlusModifier, preferencesRepository.gradeMinusModifier))
-            gradeSummaryScrollableHeaderCalculated.text = formatAverage(items
-                .filter { value -> value.average != 0.0 }
-                .map { values -> values.average }
-                .reversed() // fix average precision
-                .average()
+            gradeSummaryScrollableHeaderFinal.text = formatAverage(finalAverage)
+            gradeSummaryScrollableHeaderCalculated.text = formatAverage(calculatedAverage)
+            gradeSummaryScrollableHeaderFinalSubjectCount.text =
+                context.getString(R.string.grade_summary_from_subjects, finalItemsCount, items.size)
+            gradeSummaryScrollableHeaderCalculatedSubjectCount.text = context.getString(
+                R.string.grade_summary_from_subjects,
+                calculatedItemsCount,
+                items.size
             )
-            val finalItemsCount = items.count { it.finalGrade.matches("[0-6][+-]?".toRegex()) }
-            val calculatedItemsCount = items.count { value -> value.average != 0.0 }
-            gradeSummaryScrollableHeaderFinalSubjectCount.text = binding.root.context.getString(R.string.grade_summary_from_subjects, finalItemsCount, items.size)
-            gradeSummaryScrollableHeaderCalculatedSubjectCount.text = binding.root.context.getString(R.string.grade_summary_from_subjects, calculatedItemsCount, items.size)
         }
     }
 
@@ -76,7 +90,8 @@ class GradeSummaryAdapter @Inject constructor(
             gradeSummaryItemPredicted.text = "${item.predictedGrade} ${item.proposedPoints}".trim()
             gradeSummaryItemFinal.text = "${item.finalGrade} ${item.finalPoints}".trim()
 
-            gradeSummaryItemPointsContainer.visibility = if (item.pointsSum.isBlank()) View.GONE else View.VISIBLE
+            gradeSummaryItemPointsContainer.visibility =
+                if (item.pointsSum.isBlank()) View.GONE else View.VISIBLE
         }
     }
 
