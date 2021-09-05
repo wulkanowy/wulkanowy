57dd66f9bf294d5dc18b2d22486c54906393051f
Rafał Borcz <RafalBO99@outlook.com>
2021-09-05 03:35:00 +0200

    Account tile navigate to account details

--- app/src/main/java/io/github/wulkanowy/ui/modules/dashboard/DashboardAdapter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/dashboard/DashboardAdapter.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.github.wulkanowy.R
+import io.github.wulkanowy.data.db.entities.Student
 import io.github.wulkanowy.data.db.entities.Timetable
 import io.github.wulkanowy.data.db.entities.TimetableHeader
 import io.github.wulkanowy.databinding.ItemDashboardAccountBinding
@@ -41,7 +42,7 @@ class DashboardAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView
 
     private var lessonsTimer: Timer? = null
 
-    var onAccountTileClickListener: () -> Unit = {}
+    var onAccountTileClickListener: (Student) -> Unit = {}
 
     var onLuckyNumberTileClickListener: () -> Unit = {}
 
@@ -152,7 +153,7 @@ class DashboardAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView
             dashboardAccountItemName.text = student?.nickOrName.orEmpty()
             dashboardAccountItemSchoolName.text = student?.schoolName.orEmpty()
 
-            root.setOnClickListener { onAccountTileClickListener() }
+            root.setOnClickListener { student?.let(onAccountTileClickListener) }
         }
     }
 
