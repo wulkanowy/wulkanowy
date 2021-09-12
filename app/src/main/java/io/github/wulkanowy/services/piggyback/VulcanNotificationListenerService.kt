7a193c23cfc49434794d84538bc131d10ac01f94
Rafał Borcz <RafalBO99@outlook.com>
2021-09-12 18:13:11 +0200

    Connect listener to system

--- app/src/main/java/io/github/wulkanowy/services/piggyback/VulcanNotificationListenerService.kt
+++ app/src/main/java/io/github/wulkanowy/services/piggyback/VulcanNotificationListenerService.kt
@@ -2,10 +2,23 @@ package io.github.wulkanowy.services.piggyback
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.wulkanowy.data.repositories.PreferencesRepository
+import io.github.wulkanowy.services.sync.SyncManager
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class VulcanNotificationListenerService : NotificationListenerService() {
 
-    override fun onNotificationPosted(sbn: StatusBarNotification?) {
-        super.onNotificationPosted(sbn)
+    @Inject
+    lateinit var syncManager: SyncManager
+
+    @Inject
+    lateinit var preferenceRepository: PreferencesRepository
+
+    override fun onNotificationPosted(statusBarNotification: StatusBarNotification?) {
+        if (statusBarNotification?.packageName == "pl.edu.vulcan.hebe" && preferenceRepository.isNotificationPiggybackEnabled) {
+            syncManager.startOneTimeSyncWorker()
+        }
     }
 }
