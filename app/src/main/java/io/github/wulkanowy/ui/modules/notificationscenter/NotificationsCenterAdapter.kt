93947360b7a3ec7cecf711e4994dd97214da85cb
Rafał Borcz <RafalBO99@outlook.com>
2021-09-27 12:14:58 +0200

    Add nabigation to correct fragment in notification center

--- app/src/main/java/io/github/wulkanowy/ui/modules/notificationscenter/NotificationsCenterAdapter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/notificationscenter/NotificationsCenterAdapter.kt
@@ -15,6 +15,8 @@ import javax.inject.Inject
 class NotificationsCenterAdapter @Inject constructor() :
     ListAdapter<Notification, NotificationsCenterAdapter.ViewHolder>(DiffUtilCallback()) {
 
+    var onItemClickListener: (NotificationType) -> Unit = {}
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
         ItemNotificationsCenterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
@@ -26,22 +28,24 @@ class NotificationsCenterAdapter @Inject constructor() :
             notificationsCenterItemTitle.text = item.title
             notificationsCenterItemContent.text = item.content
             notificationsCenterItemDate.text = item.date.toFormattedString("HH:mm, d MMM")
-            notificationsCenterItemIcon.setImageResource(
-                when (item.type) {
-                    NotificationType.NEW_CONFERENCE -> R.drawable.ic_more_conferences
-                    NotificationType.NEW_EXAM -> R.drawable.ic_main_exam
-                    NotificationType.NEW_GRADE_DETAILS -> R.drawable.ic_stat_grade
-                    NotificationType.NEW_GRADE_PREDICTED -> R.drawable.ic_stat_grade
-                    NotificationType.NEW_GRADE_FINAL -> R.drawable.ic_stat_grade
-                    NotificationType.NEW_HOMEWORK -> R.drawable.ic_more_homework
-                    NotificationType.NEW_LUCKY_NUMBER -> R.drawable.ic_stat_luckynumber
-                    NotificationType.NEW_MESSAGE -> R.drawable.ic_stat_message
-                    NotificationType.NEW_NOTE -> R.drawable.ic_stat_note
-                    NotificationType.NEW_ANNOUNCEMENT -> R.drawable.ic_all_about
-                    NotificationType.PUSH -> R.drawable.ic_stat_all
-                }
-            )
+            notificationsCenterItemIcon.setImageResource(item.type.toDrawableResId())
+
+            root.setOnClickListener { onItemClickListener(item.type) }
         }
+    }
+
+    private fun NotificationType.toDrawableResId() = when (this) {
+        NotificationType.NEW_CONFERENCE -> R.drawable.ic_more_conferences
+        NotificationType.NEW_EXAM -> R.drawable.ic_main_exam
+        NotificationType.NEW_GRADE_DETAILS -> R.drawable.ic_stat_grade
+        NotificationType.NEW_GRADE_PREDICTED -> R.drawable.ic_stat_grade
+        NotificationType.NEW_GRADE_FINAL -> R.drawable.ic_stat_grade
+        NotificationType.NEW_HOMEWORK -> R.drawable.ic_more_homework
+        NotificationType.NEW_LUCKY_NUMBER -> R.drawable.ic_stat_luckynumber
+        NotificationType.NEW_MESSAGE -> R.drawable.ic_stat_message
+        NotificationType.NEW_NOTE -> R.drawable.ic_stat_note
+        NotificationType.NEW_ANNOUNCEMENT -> R.drawable.ic_all_about
+        NotificationType.PUSH -> R.drawable.ic_stat_all
     }
 
     class ViewHolder(val binding: ItemNotificationsCenterBinding) :
