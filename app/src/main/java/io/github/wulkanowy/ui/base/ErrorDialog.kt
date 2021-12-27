59a1a9db57ce417ac4e523846412a3ae3240bf1e
Mikołaj Pich <m.pich@outlook.com>
2021-12-27 16:19:10 +0100

    Use AlertDialog to draw error dialog

--- app/src/main/java/io/github/wulkanowy/ui/base/ErrorDialog.kt
+++ app/src/main/java/io/github/wulkanowy/ui/base/ErrorDialog.kt
@@ -1,5 +1,6 @@
 package io.github.wulkanowy.ui.base
 
+import android.app.Dialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.os.Bundle
@@ -12,17 +13,15 @@ import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.getSystemService
 import androidx.core.view.isGone
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.wulkanowy.R
 import io.github.wulkanowy.databinding.DialogErrorBinding
 import io.github.wulkanowy.sdk.exception.FeatureNotAvailableException
 import io.github.wulkanowy.sdk.scrapper.exception.FeatureDisabledException
 import io.github.wulkanowy.sdk.scrapper.exception.ServiceUnavailableException
-import io.github.wulkanowy.utils.AppInfo
-import io.github.wulkanowy.utils.getString
-import io.github.wulkanowy.utils.openAppInMarket
-import io.github.wulkanowy.utils.openEmailClient
-import io.github.wulkanowy.utils.openInternetBrowser
+import io.github.wulkanowy.utils.*
 import okhttp3.internal.http2.StreamResetException
 import java.io.InterruptedIOException
 import java.net.ConnectException
@@ -31,9 +30,11 @@ import java.net.UnknownHostException
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ErrorDialog : BaseDialogFragment<DialogErrorBinding>() {
+class ErrorDialog : DialogFragment() {
 
     private lateinit var error: Throwable
+
+    private var dialogView: View? = null
 
     @Inject
     lateinit var appInfo: AppInfo
@@ -49,51 +50,46 @@ class ErrorDialog : BaseDialogFragment<DialogErrorBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NO_TITLE, 0)
-        arguments?.run {
-            error = getSerializable(ARGUMENT_KEY) as Throwable
+        error = requireArguments().getSerializable(ARGUMENT_KEY) as Throwable
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val inflater = LayoutInflater.from(context)
+        val errorStacktrace = error.stackTraceToString()
+        dialogView = DialogErrorBinding.inflate(inflater).root
+        val dialog = MaterialAlertDialogBuilder(requireContext()).apply {
+            setTitle(R.string.all_details)
+            setView(dialogView)
+            setNeutralButton(R.string.about_feedback) { _, _ ->
+                openConfirmDialog { openEmailClient(errorStacktrace) }
+            }
+            setNegativeButton(android.R.string.cancel) { _, _ -> }
+            setPositiveButton(android.R.string.copy) { _, _ -> copyErrorToClipboard(errorStacktrace) }
+        }.create()
+
+        dialog.setOnShowListener {
+            dialog.getButton(AlertDialog.BUTTON_NEUTRAL).isEnabled = isErrorShouldBeReported(error)
         }
+
+        return dialog
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = DialogErrorBinding.inflate(inflater).apply { binding = this }.root
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View = requireNotNull(dialogView)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val errorStacktrace = error.stackTraceToString()
-
-        with(binding) {
-            errorDialogContent.text = errorStacktrace.replace(": ${error.localizedMessage}", "")
+        with(DialogErrorBinding.bind(requireNotNull(dialogView))) {
+            errorDialogContent.text = error.stackTraceToString()
+                .replace(": ${error.localizedMessage}", "")
             with(errorDialogHorizontalScroll) {
                 post { fullScroll(HorizontalScrollView.FOCUS_LEFT) }
-            }
-            errorDialogCopy.setOnClickListener {
-                val clip = ClipData.newPlainText("Error details", errorStacktrace)
-                activity?.getSystemService<ClipboardManager>()?.setPrimaryClip(clip)
-
-                Toast.makeText(context, R.string.all_copied, LENGTH_LONG).show()
-            }
-            errorDialogCancel.setOnClickListener { dismiss() }
-            errorDialogReport.setOnClickListener {
-                openConfirmDialog { openEmailClient(errorStacktrace) }
             }
             errorDialogHumanizedMessage.text = resources.getString(error)
             errorDialogErrorMessage.text = error.localizedMessage
             errorDialogErrorMessage.isGone = error.localizedMessage.isNullOrBlank()
-            errorDialogReport.isEnabled = when (error) {
-                is UnknownHostException,
-                is InterruptedIOException,
-                is ConnectException,
-                is StreamResetException,
-                is SocketTimeoutException,
-                is ServiceUnavailableException,
-                is FeatureDisabledException,
-                is FeatureNotAvailableException -> false
-                else -> true
-            }
         }
     }
 
@@ -126,5 +122,32 @@ class ErrorDialog : BaseDialogFragment<DialogErrorBinding>() {
                 )
             }
         )
+    }
+
+    private fun showMessage(text: String) {
+        Toast.makeText(requireContext(), text, LENGTH_LONG).show()
+    }
+
+    private fun copyErrorToClipboard(errorStacktrace: String) {
+        val clip = ClipData.newPlainText("Error details", errorStacktrace)
+        activity?.getSystemService<ClipboardManager>()?.setPrimaryClip(clip)
+        Toast.makeText(context, R.string.all_copied, LENGTH_LONG).show()
+    }
+
+    private fun isErrorShouldBeReported(error: Throwable) = when (error) {
+        is UnknownHostException,
+        is InterruptedIOException,
+        is ConnectException,
+        is StreamResetException,
+        is SocketTimeoutException,
+        is ServiceUnavailableException,
+        is FeatureDisabledException,
+        is FeatureNotAvailableException -> false
+        else -> true
+    }
+
+    override fun onDestroyView() {
+        dialogView = null
+        super.onDestroyView()
     }
 }
