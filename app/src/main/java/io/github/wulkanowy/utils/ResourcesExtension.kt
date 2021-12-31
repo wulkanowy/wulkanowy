872c87b872ec1fd1e850eacbdc8832a8285047cd
Mikołaj Pich <m.pich@outlook.com>
2021-12-31 15:32:23 +0100

    Fix cause search in different cause hierarchy

--- app/src/main/java/io/github/wulkanowy/utils/ResourcesExtension.kt
+++ app/src/main/java/io/github/wulkanowy/utils/ResourcesExtension.kt
@@ -15,8 +15,6 @@ import java.net.ConnectException
 import java.net.SocketException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
-import java.security.cert.CertPathValidatorException
-import java.security.cert.CertificateException
 import java.security.cert.CertificateExpiredException
 import java.security.cert.CertificateNotYetValidException
 import javax.net.ssl.SSLHandshakeException
@@ -35,15 +33,25 @@ fun Resources.getString(error: Throwable): String = when (error) {
     is FeatureNotAvailableException -> R.string.error_feature_not_available
     is VulcanException -> R.string.error_unknown_uonet
     is ScrapperException -> R.string.error_unknown_app
-    is SSLHandshakeException -> {
-        val isCertException = error.cause is CertificateException
-        val isPatchValidatorException = error.cause?.cause is CertPathValidatorException
-        val isNotYetValid = error.cause?.cause?.cause is CertificateNotYetValidException
-        val isExpired = error.cause?.cause?.cause is CertificateExpiredException
-
-        if (isCertException && isPatchValidatorException && (isNotYetValid || isExpired)) {
-            R.string.error_invalid_device_datetime
-        } else R.string.error_timeout
+    is SSLHandshakeException -> when {
+        isCausedByCertificateNotValidNow(error) -> R.string.error_invalid_device_datetime
+        else -> R.string.error_timeout
     }
     else -> R.string.error_unknown
 }.let { getString(it) }
+
+private fun isCausedByCertificateNotValidNow(e: Throwable?): Boolean {
+    var exception = e
+    do {
+        if (isCertificateNotValidNow(exception)) return true
+
+        exception = exception?.cause
+    } while (exception != null)
+    return false
+}
+
+private fun isCertificateNotValidNow(throwable: Throwable?): Boolean {
+    val isNotYetValid = throwable is CertificateNotYetValidException
+    val isExpired = throwable is CertificateExpiredException
+    return isNotYetValid || isExpired
+}
