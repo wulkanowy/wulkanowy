451256b8ff2227f6d6a59b14c5db82f5fde55118
Mikołaj Pich <m.pich@outlook.com>
2021-12-31 14:40:30 +0100

    Add a custom error message for ssl errors due to invalid clock setting

--- app/src/main/java/io/github/wulkanowy/utils/ResourcesExtension.kt
+++ app/src/main/java/io/github/wulkanowy/utils/ResourcesExtension.kt
@@ -15,20 +15,35 @@ import java.net.ConnectException
 import java.net.SocketException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import java.security.cert.CertPathValidatorException
+import java.security.cert.CertificateException
+import java.security.cert.CertificateExpiredException
+import java.security.cert.CertificateNotYetValidException
+import javax.net.ssl.SSLHandshakeException
 
-fun Resources.getString(error: Throwable) = when (error) {
-    is UnknownHostException -> getString(R.string.error_no_internet)
+fun Resources.getString(error: Throwable): String = when (error) {
+    is UnknownHostException -> R.string.error_no_internet
     is SocketException,
     is SocketTimeoutException,
     is InterruptedIOException,
     is ConnectException,
-    is StreamResetException -> getString(R.string.error_timeout)
-    is NotLoggedInException -> getString(R.string.error_login_failed)
-    is PasswordChangeRequiredException -> getString(R.string.error_password_change_required)
-    is ServiceUnavailableException -> getString(R.string.error_service_unavailable)
-    is FeatureDisabledException -> getString(R.string.error_feature_disabled)
-    is FeatureNotAvailableException -> getString(R.string.error_feature_not_available)
-    is VulcanException -> getString(R.string.error_unknown_uonet)
-    is ScrapperException -> getString(R.string.error_unknown_app)
-    else -> getString(R.string.error_unknown)
-}
+    is StreamResetException -> R.string.error_timeout
+    is NotLoggedInException -> R.string.error_login_failed
+    is PasswordChangeRequiredException -> R.string.error_password_change_required
+    is ServiceUnavailableException -> R.string.error_service_unavailable
+    is FeatureDisabledException -> R.string.error_feature_disabled
+    is FeatureNotAvailableException -> R.string.error_feature_not_available
+    is VulcanException -> R.string.error_unknown_uonet
+    is ScrapperException -> R.string.error_unknown_app
+    is SSLHandshakeException -> {
+        val isCertException = error.cause is CertificateException
+        val isPatchValidatorException = error.cause?.cause is CertPathValidatorException
+        val isNotYetValid = error.cause?.cause?.cause is CertificateNotYetValidException
+        val isExpired = error.cause?.cause?.cause is CertificateExpiredException
+
+        if (isCertException && isPatchValidatorException && (isNotYetValid || isExpired)) {
+            R.string.error_invalid_device_datetime
+        } else R.string.error_timeout
+    }
+    else -> R.string.error_unknown
+}.let { getString(it) }
