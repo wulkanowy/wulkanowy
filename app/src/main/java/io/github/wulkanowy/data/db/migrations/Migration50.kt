76a74680e4b74227761d82f00af86f84d7d1cda1
Rafał Borcz <RafalBO99@outlook.com>
2022-07-01 03:50:27 +0200

    Chane db number

--- app/src/main/java/io/github/wulkanowy/data/db/migrations/Migration50.kt
+++ app/src/main/java/io/github/wulkanowy/data/db/migrations/Migration50.kt
@@ -3,7 +3,7 @@ package io.github.wulkanowy.data.db.migrations
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-class Migration49 : Migration(48, 49) {
+class Migration50 : Migration(48, 49) {
 
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("DROP TABLE IF EXISTS MobileDevices")
