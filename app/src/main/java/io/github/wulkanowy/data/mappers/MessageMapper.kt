0c7cac4ee22d2e6a57c39987aae7842d939e793c
Mikołaj Pich <m.pich@outlook.com>
2022-11-17 21:47:00 +0100

    Add additional log stmt to mailbox matching in all mailbox load

--- app/src/main/java/io/github/wulkanowy/data/mappers/MessageMapper.kt
+++ app/src/main/java/io/github/wulkanowy/data/mappers/MessageMapper.kt
@@ -2,16 +2,24 @@ package io.github.wulkanowy.data.mappers
 
 import io.github.wulkanowy.data.db.entities.*
 import io.github.wulkanowy.sdk.pojo.MailboxType
+import timber.log.Timber
 import io.github.wulkanowy.sdk.pojo.Message as SdkMessage
 import io.github.wulkanowy.sdk.pojo.MessageAttachment as SdkMessageAttachment
 import io.github.wulkanowy.sdk.pojo.Recipient as SdkRecipient
 
-fun List<SdkMessage>.mapToEntities(student: Student, mailbox: Mailbox?, allMailboxes: List<Mailbox>) = map {
+fun List<SdkMessage>.mapToEntities(
+    student: Student,
+    mailbox: Mailbox?,
+    allMailboxes: List<Mailbox>
+): List<Message> = map {
     Message(
         messageGlobalKey = it.globalKey,
         mailboxKey = mailbox?.globalKey ?: allMailboxes.find { box ->
             box.fullName == it.mailbox
-        }?.globalKey!!,
+        }?.globalKey ?: let { _ ->
+            Timber.e("Messages cannot be matched to the mailbox! ${it.mailbox} !in $allMailboxes")
+            "unknown"
+        },
         email = student.email,
         messageId = it.id,
         correspondents = it.correspondents,
